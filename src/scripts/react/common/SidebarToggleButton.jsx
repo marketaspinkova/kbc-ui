e7e0afa85dea2bee3ca20e07d6f2e442d24b3c15
Jakub Kotek <blaztar@gmail.com>
2018-09-27 18:56:57 +0200

    tiny improvement

--- src/scripts/react/common/SidebarToggleButton.jsx
+++ src/scripts/react/common/SidebarToggleButton.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import _ from 'underscore';
 
 export default React.createClass({
   _toggleSidebar() {
     document.querySelectorAll('.sidebar-offset-row')[0].classList.toggle('active');
+  },
+
+  componentDidMount() {
+    const links = document.querySelectorAll('.nav.nav-sidebar > li > a');
+    const sidebarRow = document.querySelectorAll('.sidebar-offset-row')[0];
+
+    _.map(links, link =>
+      link.addEventListener('click', () => {
+        sidebarRow.classList.remove('active');
+      })
+    );
   },
 
   render() {
