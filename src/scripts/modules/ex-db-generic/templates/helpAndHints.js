756f0768d8f3e270c2ab6e8663dceba56371de49
pivnicek <marc@keboola.com>
2018-10-01 13:59:26 +0200

    piv-mssql-comment

--- src/scripts/modules/ex-db-generic/templates/helpAndHints.js
+++ src/scripts/modules/ex-db-generic/templates/helpAndHints.js
@@ -9,7 +9,9 @@ export function getQueryEditorPlaceholder(componentId) {
 }
 
 const queryEditorHelp = {
-  'keboola.ex-db-oracle': 'Please do not put semicolons at the end of the query.'
+  'keboola.ex-db-oracle': 'Please do not put semicolons at the end of the query.',
+  'keboola.ex-db-mssql': 'Please note that as of version 4.0, DATETIME fields will be exported with milliseconds.' +
+  '  If you\'d like to export without milliseconds please cast your column as "CONVERT(DATETIME2(0), my_column)"'
 };
 
 export function getQueryEditorHelpText(componentId) {
