6be27234a971f7696e0de9c1262e857dd5559295
pivnicek <marc@keboola.com>
2018-10-02 17:04:37 +0200

    add comment about escaping quotes

--- src/scripts/modules/ex-db-generic/templates/helpAndHints.js
+++ src/scripts/modules/ex-db-generic/templates/helpAndHints.js
@@ -11,7 +11,9 @@ export function getQueryEditorPlaceholder(componentId) {
 const queryEditorHelp = {
   'keboola.ex-db-oracle': 'Please do not put semicolons at the end of the query.',
   'keboola.ex-db-mssql': 'Please note that as of Oct 1, 2018, DATETIME fields will be exported with milliseconds.' +
-  '  If you\'d like to export without milliseconds please cast your column as "CONVERT(DATETIME2(0), my_column)"'
+  '  If you\'d like to export without milliseconds please cast your column as "CONVERT(DATETIME2(0), my_column)"' +
+  'Also if you are exporting strings that may contain quotes they should be escaped ' +
+  '"char(34) + REPLACE([my_varchar_column], char(34), char(34) + char(34)) + char(34)"'
 };
 
 export function getQueryEditorHelpText(componentId) {
