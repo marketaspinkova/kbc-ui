688cb4e31aa5ea8f677b5c4d294b17bb15611246
Vladimír Kriška <vlado@keboola.com>
2018-10-02 15:17:03 +0200

    Add "eslint-disable react/no-multi-comp" due to false positive

--- src/scripts/modules/transformations/Routes.js
+++ src/scripts/modules/transformations/Routes.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-multi-comp */
+// ^ due to false positive
 import React from 'react';
 
 import TransformationsIndex from './react/pages/transformations-index/TransformationsIndex';
