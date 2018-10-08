6b32ccb36ff66c2c5a02335671c1916bcdaa96cd
Vladimír Kriška <vlado@keboola.com>
2018-10-08 15:17:44 +0200

    Use ExternalLink for link to docs

--- src/scripts/modules/ex-db-generic/templates/helpAndHints.js
+++ src/scripts/modules/ex-db-generic/templates/helpAndHints.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ExternalLink } from '@keboola/indigo-ui';
 
 const queryEditorPlaceholder = {
   'default': 'e.g. SELECT "id", "name" FROM "myTable"',
@@ -13,7 +14,7 @@ const queryEditorHelp = {
   'keboola.ex-db-oracle': 'Please do not put semicolons at the end of the query.',
   'keboola.ex-db-mssql': <div>From Oct 1, 2018, DATETIME fields will be exported with milliseconds.<br/>
     If you'd like to export without milliseconds please cast your column as <code>CONVERT(DATETIME2(0), my_column)</code><br/>
-    See the <a href="https://help.keboola.com/extractors/database/sqldb/#ms-sql-server-advanced-mode">documentation</a>
+    See the <ExternalLink href="https://help.keboola.com/extractors/database/sqldb/#ms-sql-server-advanced-mode">documentation</ExternalLink>
     &nbsp;for further help.</div>
 };
 
