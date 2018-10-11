4bf71db45bb10ec40c646f16bfc684f7a5d2f25a
Tomas Kacur <tomas.kacur@keboola.com>
2018-10-11 16:37:47 +0200

    fix wrgdv3 routes breadcrumbs names

--- src/scripts/modules/gooddata-writer-v3/routes.js
+++ src/scripts/modules/gooddata-writer-v3/routes.js
@@ -1,6 +1,8 @@
 import Index from './react/pages/index/Index';
 import Table from './react/pages/table/Table';
 
+import InstalledComponentsStore from '../components/stores/InstalledComponentsStore';
+
 import installedComponentsActions from '../components/InstalledComponentsActionCreators';
 import storageActions from '../components/StorageActionCreators';
 // import jobsActionCreators from '../jobs/ActionCreators';
@@ -10,11 +12,14 @@ import {createTablesRoute} from '../table-browser/routes';
 
 const componentId = 'keboola.gooddata-writer';
 export default {
-
   name: componentId,
   path: ':config',
   isComponent: true,
   defaultRouteHandler: Index,
+  title: (routerState) => {
+    const configId = routerState.getIn(['params', 'config']);
+    return InstalledComponentsStore.getConfig(componentId, configId).get('name');
+  },
   requireData: [
     (params) => installedComponentsActions.loadComponentConfigData(componentId, params.config),
     () => storageActions.loadTables(),
@@ -29,7 +34,8 @@ export default {
     {
       name: componentId + '-table',
       path: 'table/:table',
-      defaultRouteHandler: Table
+      defaultRouteHandler: Table,
+      title: (routerState) => routerState.getIn(['params', 'table'])
     }
   ]
 };
