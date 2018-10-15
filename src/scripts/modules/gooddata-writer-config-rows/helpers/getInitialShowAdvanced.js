b429efa45fe4fdebe88f0c364d59fe1c1ab46e06
Tomas Kacur <tomas.kacur@keboola.com>
2018-10-15 16:14:20 +0200

    remove gooddata config-rows module

--- src/scripts/modules/gooddata-writer-config-rows/helpers/getInitialShowAdvanced.js
+++
@@ -1,7 +0,0 @@
-export default function getInitialShowAdvanced(columns) {
-  return !!columns.find(column => {
-    return !!(column.identifier ||
-              column.identifierLabel ||
-              column.identifierSortLabel);
-  });
-}
