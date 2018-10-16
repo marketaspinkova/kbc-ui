0db1018eb60ea81068eeb7821f1d5c3bd204ceb6
janmichek <jan@keboola.com>
2018-10-16 11:33:25 +0200

    replaces email in EmptyList

--- src/scripts/react/layout/project-select/EmptyList.jsx
+++ src/scripts/react/layout/project-select/EmptyList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import contactSupport from '../../../utils/contactSupport';
 
 export default React.createClass({
 
@@ -7,7 +8,7 @@ export default React.createClass({
       <div>
         <h2 style={{margin: '0 0 10px 0'}}>Welcome to Keboola Connection</h2>
         <p>You are not member of any project yet.
-          Please <a href="mailto:support@keboola.com">contact us</a> to get started.
+          Please <a href="" onClick={contactSupport}>contact us</a> to get started.
         </p>
       </div>
     );
