7c027708bfcf054f8cdd50ef14e3418aee245346
janmichek <jan@keboola.com>
2018-10-17 18:10:04 +0200

    replaces contact support link fo Button

--- src/scripts/react/layout/project-select/EmptyList.jsx
+++ src/scripts/react/layout/project-select/EmptyList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import contactSupport from '../../../utils/contactSupport';
+import {Button} from 'react-bootstrap';
 
 export default React.createClass({
 
@@ -8,8 +9,11 @@ export default React.createClass({
       <div>
         <h2 style={{margin: '0 0 10px 0'}}>Welcome to Keboola Connection</h2>
         <p>You are not member of any project yet.
-          Please <a href="" onClick={contactSupport}>contact us</a> to get started.
+          Please contact us to get started.
         </p>
+        <Button block bsStyle="primary" onClick={contactSupport}>
+          Contact Support
+        </Button>
       </div>
     );
   }
