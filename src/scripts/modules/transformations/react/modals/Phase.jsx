c1e80f5d26315e6216f1cc46441ec43afd3c7d4e
Jakub Kotek <blaztar@gmail.com>
2018-10-18 09:19:50 +0200

    Prepare for adding form.

--- src/scripts/modules/transformations/react/modals/Phase.jsx
+++ src/scripts/modules/transformations/react/modals/Phase.jsx
@@ -1,6 +1,6 @@
-import React, {PropTypes} from 'react';
+import React, { PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import {Modal} from 'react-bootstrap';
+import { Form, FormControl, FormGroup, Modal, Col } from 'react-bootstrap';
 import Tooltip from './../../../../react/common/Tooltip';
 import ConfirmButtons from '../../../../react/common/ConfirmButtons';
 import actionCreators from '../../ActionCreators';
@@ -39,45 +39,47 @@ export default React.createClass({
   render() {
     return (
       <span>
-        { this.renderOpenButton() }
+        {this.renderOpenButton()}
+
         <Modal onHide={this.close} show={this.state.showModal}>
-          <Modal.Header closeButton={true}>
-            <Modal.Title>
-              Transformation Phase
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="form-horizontal">
-              <div className="form-group">
-                <div className="col-sm-offset-1 col-sm-9">
+          <Form onSubmit={this.handleSubmit} horizontal>
+            <Modal.Header closeButton={true}>
+              <Modal.Title>Transformation Phase</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <FormGroup>
+                <Col smOffset={1} sm={9}>
                   <p>
-                    <a href="https://help.keboola.com/manipulation/transformations/#phases">Phase</a> is a set of transformations.
+                    <a href="https://help.keboola.com/manipulation/transformations/#phases">Phase</a> is a set of
+                    transformations.
                   </p>
                   <p className="help-block">
-                    Phases may be used to divide transformations into logical blocks, transfer data between transformations, transformation engines and remote transformations.
+                    Phases may be used to divide transformations into logical blocks, transfer data between
+                    transformations, transformation engines and remote transformations.
                   </p>
                   <p>
-                    Phase # <input
+                    Phase #{' '}
+                    <FormControl
                       type="number"
-                      className="form-control"
+                      min={1}
                       value={parseInt(this.state.phase, 10)}
                       onChange={this.handlePhaseChange}
                       disabled={this.state.isSaving}
-                      style={{width: '50px', display: 'inline-block'}}
+                      style={{ width: '50px', display: 'inline-block' }}
                     />
                   </p>
-                </div>
-              </div>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <ConfirmButtons
-              isSaving={this.state.isSaving}
-              onCancel={this.close}
-              onSave={this.handleSave}
-              isDisabled={!this.isValid()}
-            />
-          </Modal.Footer>
+                </Col>
+              </FormGroup>
+            </Modal.Body>
+            <Modal.Footer>
+              <ConfirmButtons
+                isSaving={this.state.isSaving}
+                onCancel={this.close}
+                onSave={this.handleSubmit}
+                isDisabled={!this.isValid()}
+              />
+            </Modal.Footer>
+          </Form>
         </Modal>
       </span>
     );
@@ -94,9 +96,7 @@ export default React.createClass({
       return (
         <Tooltip tooltip="Change Transformation Phase" placement="top">
           <span onClick={this.open} className="label kbc-label-rounded-small label-default kbc-cursor-pointer">
-            Phase: {this.props.transformation.get('phase')}
-            {' '}
-            <span className="kbc-icon-pencil"/>
+            Phase: {this.props.transformation.get('phase')} <span className="kbc-icon-pencil" />
           </span>
         </Tooltip>
       );
@@ -112,27 +112,23 @@ export default React.createClass({
     });
   },
 
-  handleSave() {
+  handleSubmit(e) {
+    e.preventDefault();
     this.setState({
       isSaving: true
     });
     actionCreators
-      .changeTransformationProperty(this.props.bucketId,
-        this.props.transformation.get('id'),
-        'phase',
-        this.state.phase
-      )
+      .changeTransformationProperty(this.props.bucketId, this.props.transformation.get('id'), 'phase', this.state.phase)
       .then(() => this.close())
-      .catch((e) => {
+      .catch(error => {
         this.setState({
           isSaving: false
         });
-        throw e;
+        throw error;
       });
   },
 
   isValid() {
     return this.state.phase >= 1;
   }
-
 });
