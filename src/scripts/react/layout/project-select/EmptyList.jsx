b09dc57ec125da55ebc0307bac2fc2b7eb74771e
Vladimír Kriška <vlado@keboola.com>
2018-10-23 12:10:49 +0200

    Unify texts; Add link to promo codes

--- src/scripts/react/layout/project-select/EmptyList.jsx
+++ src/scripts/react/layout/project-select/EmptyList.jsx
@@ -8,7 +8,20 @@ export default React.createClass({
         <h2 style={{margin: '0 0 10px 0'}}>Welcome to Keboola Connection</h2>
         <p>
           You are not member of any project yet.
-          Please{' '}
+        </p>
+        <div className="kbc-no-projects">
+          <a href="/admin/account/promo-codes" className="btn btn-lg btn-block btn-success text-left action-button">
+            <span className="raquo">&raquo;</span>
+            <strong className="h2">Enter promo code</strong><br/>
+            <span className="small">
+              Select this option if you have a promo code.<br/>
+              A new project will be created automatically.<br/>
+            </span>
+          </a>
+        </div>
+        <hr />
+        <p className="text-center">
+          Or{' '}
           <button className="btn btn-link btn-link-inline" onClick={contactSupport}>
             contact us
           </button>
