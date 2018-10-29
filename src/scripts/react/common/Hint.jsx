642904dd38a5866165c469103e3b710dde75130f
Jakub Kotek <blaztar@gmail.com>
2018-10-29 14:13:39 +0100

    add id to popover

--- src/scripts/react/common/Hint.jsx
+++ src/scripts/react/common/Hint.jsx
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { slugify } from 'underscore.string';
 import {OverlayTrigger, Popover} from 'react-bootstrap';
 
 export default React.createClass({
@@ -19,7 +20,7 @@ export default React.createClass({
 
   popover() {
     return (
-      <Popover title={this.props.title}>
+      <Popover id={`hint-${slugify(this.props.title)}`} title={this.props.title}>
         {this.props.children}
       </Popover>
     );
