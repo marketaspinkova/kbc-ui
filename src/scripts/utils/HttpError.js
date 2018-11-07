defad9ce730e551391d7150bb3bd54bdf3d74ec1
Jakub Kotek <blaztar@gmail.com>
2018-11-07 12:44:30 +0100

    better httpError implementation

--- src/scripts/utils/HttpError.js
+++ src/scripts/utils/HttpError.js
@@ -1,19 +1,10 @@
-/* eslint-disable */
 class HttpError extends Error {
   constructor(response) {
-    {
-      // Hack: trick Babel/TypeScript into allowing this before super.
-      if (false) {
-        super();
-      }
-      let thisFn = (() => {
-        return this;
-      }).toString();
-      let thisName = thisFn.slice(thisFn.indexOf('return') + 6 + 1, thisFn.indexOf(';')).trim();
-      eval(`${thisName} = this;`);
-    }
+    super();
     this.response = response;
-    this.message = this.response.body != null ? this.response.body.error : undefined;
+    if (this.response.body) {
+      this.message = this.response.body.error;
+    }
   }
 }
 
