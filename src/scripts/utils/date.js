52b65bc811bf14a81b27f76b2f204ba5b20effbb
Jakub Kotek <blaztar@gmail.com>
2018-11-08 19:51:57 +0100

    fix date utils

--- src/scripts/utils/date.js
+++ src/scripts/utils/date.js
@@ -1,7 +1,9 @@
 import moment from 'moment';
 
+export const format = (date, scheme = 'YYYY-MM-DD HH:mm:ss') => {
+  return moment(date).format(scheme);
+};
+
 export default {
-  format(date, format = 'YYYY-MM-DD HH:mm:ss') {
-    return moment(date).format(format);
-  }
+  format
 };
