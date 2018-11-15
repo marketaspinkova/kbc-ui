a5a3ae8061f3a96461b003003f401ae8064b412b
Tomas Kacur <tomas.kacur@keboola.com>
2018-11-15 15:26:47 +0100

    match by words: don't preserve order of matches in result match

--- src/scripts/utils/matchByWords.js
+++ src/scripts/utils/matchByWords.js
@@ -1,12 +1,6 @@
 export default function(inputString, query) {
   const queryWords = query.trim().split(' ');
-  const result = queryWords.reduce(
-    (memo, word) => {
-      const fromIndex = memo.lastMatch && inputString.indexOf(word, memo.fromIndex);
-      const lastMatch = memo.lastMatch && fromIndex >= 0;
-      return { lastMatch, fromIndex };
-    },
-    { fromIndex: 0, lastMatch: true }
-  );
-  return result.lastMatch;
+  return queryWords.reduce((memo, word) => {
+    return memo && inputString.indexOf(word) >= 0;
+  }, true);
 }
--- src/scripts/utils/matchByWords.spec.js
+++ src/scripts/utils/matchByWords.spec.js
@@ -24,12 +24,12 @@ describe('test matchByWords', () => {
 
   it('should match words in order', () => {
     assert.ok(matchByWords('some input param string', 'some param'));
+    assert.ok(matchByWords('some input param string', ' string param'));
     assert.ok(matchByWords('some input param string', '  some   param  '));
     assert.ok(matchByWords('some input param string', ' some  string   '));
   });
 
   it('should not match more words', () => {
-    assert.ok(!matchByWords('some input param string', ' string param'));
     assert.ok(!matchByWords('some input param string', '  blabla   param  '));
     assert.ok(!matchByWords('some input param string', ' some  blablag   '));
     assert.ok(!matchByWords('some input param string', ' total  blablag   '));
@@ -42,15 +42,16 @@ describe('test matchByWords', () => {
   });
 
   it('should fuzzy match by single characters', () => {
+    assert.ok(matchByWords('some input param string', ' string p r m str'));
     assert.ok(matchByWords('some input param string', ' p r m str'));
     assert.ok(matchByWords('some input param string', '  param   st i  '));
     assert.ok(matchByWords('some input param string', ' som  s g '));
+    assert.ok(matchByWords('some input param string', ' i g put'));
+
+    assert.ok(matchByWords('some input param string', ' input  e'));
   });
 
   it('should not fuzzy match by single characters', () => {
-    assert.ok(!matchByWords('some input param string', ' string p r m str'));
-    assert.ok(!matchByWords('some input param string', ' i g put'));
     assert.ok(!matchByWords('some input param string', ' igput  s'));
-    assert.ok(!matchByWords('some input param string', ' input  e'));
   });
 });
