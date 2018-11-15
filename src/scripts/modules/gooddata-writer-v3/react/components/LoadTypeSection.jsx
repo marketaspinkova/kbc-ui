c063216dd41d82fa43899597643c6a9befa94e87
Tomas Kacur <tomas.kacur@keboola.com>
2018-11-15 17:37:35 +0100

    hide changed in last and grain input for full load option

--- src/scripts/modules/gooddata-writer-v3/react/components/LoadTypeSection.jsx
+++ src/scripts/modules/gooddata-writer-v3/react/components/LoadTypeSection.jsx
@@ -50,39 +50,43 @@ export default React.createClass({
             </HelpBlock>
           </Col>
         </FormGroup>
-        <FormGroup>
-          <Col componentClass={ControlLabel} sm={4}>
-            Changed In Last
-          </Col>
-          <Col sm={8}>
-            <ChangedSinceInput
-              value={value.changedSince}
-              onChange={(newValue) => this.props.onChange({changedSince: newValue})}
-              disabled={disabled || !isIncremental}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup>
-          <Col sm={4} componentClass={ControlLabel}>
-            Fact Grain
-          </Col>
-          <Col sm={8}>
-            <Select
-              placeholder="Select at least 2 columns"
-              multi={true}
-              disabled={disabled || !isIncremental || value.hasConnectionPoint}
-              options={value.grainColumns.map(column => ({value: column, label: column}))}
-              value={value.grain}
-              onChange={newColumns => onChange({grain: newColumns.map(column => column.value)})}
-            />
-            <HelpBlock>
-              <ExternalLink href="https://developer.gooddata.com/article/set-fact-table-grain">
-                Fact grain
-              </ExternalLink>
-              {' '} columns help to avoid of duplicates records in GoodData dataset without connection point. Specify at least two attribute, reference or date type columns.
-            </HelpBlock>
-          </Col>
-        </FormGroup>
+        {isIncremental &&
+         <FormGroup>
+           <Col componentClass={ControlLabel} sm={4}>
+             Changed In Last
+           </Col>
+           <Col sm={8}>
+             <ChangedSinceInput
+               value={value.changedSince}
+               onChange={(newValue) => this.props.onChange({changedSince: newValue})}
+               disabled={disabled}
+             />
+           </Col>
+         </FormGroup>
+        }
+        {isIncremental && !value.hasConnectionPoint &&
+         <FormGroup>
+           <Col sm={4} componentClass={ControlLabel}>
+             Fact Grain
+           </Col>
+           <Col sm={8}>
+             <Select
+               placeholder="Select at least 2 columns"
+               multi={true}
+               disabled={disabled}
+               options={value.grainColumns.map(column => ({value: column, label: column}))}
+               value={value.grain}
+               onChange={newColumns => onChange({grain: newColumns.map(column => column.value)})}
+             />
+             <HelpBlock>
+               <ExternalLink href="https://developer.gooddata.com/article/set-fact-table-grain">
+                 Fact grain
+               </ExternalLink>
+               {' '} columns help to avoid of duplicates records in GoodData dataset without connection point. Specify at least two attribute, reference or date type columns.
+             </HelpBlock>
+           </Col>
+         </FormGroup>
+        }
       </Form>
     );
   }
