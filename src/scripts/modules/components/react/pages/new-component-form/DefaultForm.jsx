b0cd6359f2f46801bd8f764a829e96758f49765f
Jakub Kotek <blaztar@gmail.com>
2018-11-16 16:39:28 +0100

    transform to jsx

---
+++ src/scripts/modules/components/react/pages/new-component-form/DefaultForm.jsx
@@ -0,0 +1,134 @@
+import React from 'react';
+import ComponentIcon from '../../../../../react/common/ComponentIcon';
+import ComponentName from '../../../../../react/common/ComponentName';
+import { Input } from './../../../../../react/common/KbcBootstrap';
+import AppVendorInfo from './AppVendorInfo';
+import AppUsageInfo from './AppUsageInfo';
+import { Modal, ButtonToolbar, Button } from 'react-bootstrap';
+import { Loader } from '@keboola/indigo-ui';
+
+export default React.createClass({
+  propTypes: {
+    component: React.PropTypes.object.isRequired,
+    configuration: React.PropTypes.object.isRequired,
+    onCancel: React.PropTypes.func.isRequired,
+    onSave: React.PropTypes.func.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    isValid: React.PropTypes.bool.isRequired,
+    isSaving: React.PropTypes.bool.isRequired,
+    onClose: React.PropTypes.func.isRequired
+  },
+
+  _handleChange(propName, event) {
+    this.props.onChange(this.props.configuration.set(propName, event.target.value));
+  },
+
+  render() {
+    return (
+      <div>
+        <Modal.Header closeButton={true} onHide={this.props.onClose} className="modal-configuration-header">
+          <div className="row">
+            <div className="col-xs-3">
+              <ComponentIcon component={this.props.component} className="modal-configuration-icon" size="64" />
+            </div>
+            <div className="col-xs-9">
+              <h2 className="modal-configuration-title">
+                <ComponentName component={this.props.component} />
+              </h2>
+              <p>{this.props.component.get('description')}</p>
+            </div>
+          </div>
+        </Modal.Header>
+        <Modal.Body className="modal-configuration-body">
+          <form className="form-horizontal" onSubmit={this._handleSubmit}>
+            <Input
+              type="text"
+              label="Name"
+              ref="name"
+              autoFocus={true}
+              value={this.props.configuration.get('name')}
+              placeholder={`My ${this.props.component.get('name')} ${this.props.component.get('type')}`}
+              labelClassName="col-xs-3"
+              wrapperClassName="col-xs-9"
+              onChange={this._handleChange.bind(this, 'name')}
+              disabled={this.props.isSaving}
+            />
+            <Input
+              type="textarea"
+              label="Description"
+              value={this.props.configuration.get('description')}
+              labelClassName="col-xs-3"
+              wrapperClassName="col-xs-9"
+              onChange={this._handleChange.bind(this, 'description')}
+              disabled={this.props.isSaving}
+            />
+            {this._is3rdPartyApp() && this._renderAppVendorInfo()}
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <ButtonToolbar>
+            {this.props.isSaving && (
+              <span>
+                <Loader />{' '}
+              </span>
+            )}
+            <Button bsStyle="link" disabled={this.props.isSaving} onClick={this.props.onCancel}>
+              Cancel
+            </Button>
+            <Button
+              bsStyle="success"
+              disabled={!(this.props.isValid && this._isLicenseAgreed()) || this.props.isSaving}
+              onClick={this.props.onSave}
+            >
+              Create Configuration
+            </Button>
+          </ButtonToolbar>
+        </Modal.Footer>
+      </div>
+    );
+  },
+
+  _renderAppVendorInfo() {
+    return (
+      <AppVendorInfo
+        component={this.props.component}
+        licenseAgreed={this._isLicenseAgreed()}
+        handleAgreedLicense={this._setAgreedLicense}
+      />
+    );
+  },
+
+  _renderAppUsageInfo() {
+    return (
+      <div className="form-group">
+        <div className="col-xs-12">
+          <AppUsageInfo component={this.props.component} />
+        </div>
+      </div>
+    );
+  },
+
+  _is3rdPartyApp() {
+    return this.props.component.get('flags').contains('3rdParty');
+  },
+
+  _isLicenseAgreed() {
+    // if is not 3rdparty app then license is always agreed by default
+    if (!this._is3rdPartyApp()) {
+      return true;
+    }
+
+    return this.props.configuration.get('agreed') || false;
+  },
+
+  _setAgreedLicense(checked) {
+    this.props.onChange(this.props.configuration.set('agreed', checked));
+  },
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    if (this.props.isValid && this._isLicenseAgreed()) {
+      return this.props.onSave();
+    }
+  }
+});
---
+++ src/scripts/modules/components/react/pages/new-component-form/ManualConfigurationFrom.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import contactSupport from '../../../../../utils/contactSupport';
+import ComponentIcon from '../../../../../react/common/ComponentIcon';
+import ComponentName from '../../../../../react/common/ComponentName';
+import { Modal, Button, ButtonToolbar } from 'react-bootstrap';
+
+export default React.createClass({
+  propTypes: {
+    component: React.PropTypes.object.isRequired,
+    configuration: React.PropTypes.object.isRequired,
+    onCancel: React.PropTypes.func.isRequired,
+    onClose: React.PropTypes.func.isRequired
+  },
+
+  render() {
+    return (
+      <div>
+        <Modal.Header closeButton={true} onHide={this.props.onClose} className="modal-configuration-header">
+          <div className="row">
+            <div className="col-xs-3">
+              <ComponentIcon component={this.props.component} className="modal-configuration-icon" size="64" />
+            </div>
+            <div className="col-xs-9">
+              <h2 className="modal-configuration-title">
+                <ComponentName component={this.props.component} />
+              </h2>
+              <p>{this.props.component.get('description')}</p>
+            </div>
+          </div>
+        </Modal.Header>
+        <Modal.Body className="modal-configuration-body">
+          <p>{this._text()}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <ButtonToolbar>
+            <Button bsStyle="link" onClick={this.props.onClose}>
+              Close
+            </Button>
+            <Button bsStyle="success" onClick={this._contactSupport}>
+              Contact Support
+            </Button>
+          </ButtonToolbar>
+        </Modal.Footer>
+      </div>
+    );
+  },
+
+  _text() {
+    switch (this.props.component.get('type')) {
+      case 'writer':
+        return 'This writer has to be configured manually, please contact our support for assistance.';
+      case 'extractor':
+        return 'This extractor has to be configured manually, please contact our support for assistance.';
+      default:
+        break;
+    }
+  },
+
+  _contactSupport() {
+    const name = this.props.component.get('name');
+    const type = this.props.component.get('type');
+
+    return contactSupport({
+      subject: `${name} ${type} configuration assistance request`,
+      type: 'project'
+    });
+  }
+});
