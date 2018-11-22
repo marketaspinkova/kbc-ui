61837423a8c15d036cd8a6e1338e0c585c1b4d7d
Jakub Kotek <blaztar@gmail.com>
2018-11-22 08:12:35 +0100

    add run id information

--- src/scripts/react/common/JobRunId.jsx
+++ src/scripts/react/common/JobRunId.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
 export default React.createClass({
-
   propTypes: {
     runId: React.PropTypes.string
   },
 
   render() {
-    return (
-      <span style={{wordBreak: 'break-all'}}>
-        {this.props.runId}
-      </span>
-    );
+    return <span className="kbc-break-all">{this.props.runId}</span>;
   }
-
 });
