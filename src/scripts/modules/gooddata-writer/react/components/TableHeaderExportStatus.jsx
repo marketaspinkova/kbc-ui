93c7de0a7e59dbf39a8b65fca7c6ac622dd5ad5a
Vladimír Kriška <vlado@keboola.com>
2018-11-23 18:17:57 +0100

    Convert common gooddata writer components to jsx

---
+++ src/scripts/modules/gooddata-writer/react/components/TableHeaderExportStatus.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Loader } from '@keboola/indigo-ui';
+
+import createStoreMixin from '../../../../react/mixins/createStoreMixin';
+import RoutesStore from '../../../../stores/RoutesStore';
+import Tooltip from '../../../../react/common/Tooltip';
+
+import goodDataWriterStore from '../../store';
+
+export default React.createClass({
+  mixins: [createStoreMixin(goodDataWriterStore)],
+
+  componentWillReceiveProps() {
+    return this.setState(this.getStateFromStores());
+  },
+
+  getStateFromStores() {
+    const configId = RoutesStore.getCurrentRouteParam('config');
+    const tableId = RoutesStore.getCurrentRouteParam('table');
+
+    return {
+      table: goodDataWriterStore.getTable(configId, tableId),
+      configurationId: configId
+    };
+  },
+
+  render() {
+    let iconClass;
+    if (this._isExported()) {
+      iconClass = 'text-success';
+    } else {
+      iconClass = 'text-muted';
+    }
+
+    return (
+      <small>
+        <Tooltip tooltip={this._tooltip()}>
+          <span className={`fa fa-upload ${iconClass}`} />
+        </Tooltip>
+        {this._loader()}
+      </small>
+    );
+  },
+
+  _loader() {
+    const isSaving = this.state.table.get('savingFields').contains('isExported');
+    const isReset = this.state.table.get('pendingActions').contains('resetTable');
+    const isSync = this.state.table.get('pendingActions').contains('syncTable');
+    if (isSaving || isReset || isSync) {
+      return (
+        <span>
+          {' '}
+          <Loader />
+        </span>
+      );
+    }
+  },
+
+  _tooltip() {
+    if (this._isExported()) {
+      return 'Table is exported';
+    } else {
+      return 'Table is not exported';
+    }
+  },
+
+  _isExported() {
+    return this.state.table.getIn(['data', 'isExported']);
+  }
+});
