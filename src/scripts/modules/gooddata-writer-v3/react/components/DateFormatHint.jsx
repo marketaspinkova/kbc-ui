3e311e5c6a5e0529408e8dfe259e91264148b7bf
Tomas Kacur <tomas.kacur@keboola.com>
2018-11-27 19:18:29 +0100

    wrgdv3: render date format hint for date type columns

---
+++ src/scripts/modules/gooddata-writer-v3/react/components/DateFormatHint.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Popover, OverlayTrigger } from 'react-bootstrap';
+
+export default React.createClass({
+  render() {
+    return (
+      <OverlayTrigger overlay={this._renderPopover()}>
+        <span className="fa fa-question-circle" />
+      </OverlayTrigger>
+    );
+  },
+
+  _renderPopover() {
+    return (
+      <Popover title="Supported Date Formats">
+        <ul>
+          <li>yyyy – year (e.g. 2010)</li>
+          <li>MM – month (01 - 12)</li>
+          <li>dd – day (01 - 31)</li>
+          <li>hh – hour (01 - 12)</li>
+          <li>HH – hour 24 format (00 - 23)</li>
+          <li>mm – minutes (00 - 59)</li>
+          <li>ss – seconds (00 - 59)</li>
+          <li>kk/kkkk – microseconds or fractions of seconds (00-99, 000-999, 0000-9999)</li>
+          <li>GOODDATA - number of days since 1900-01-01</li>
+        </ul>
+      </Popover>
+    );
+  }
+});
