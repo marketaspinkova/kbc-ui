40f8909f64d0d99d630319c9d4b012d00919bca1
janmichek <jan@keboola.com>
2018-11-29 14:16:11 +0100

    init

--- src/scripts/modules/orchestrations/react/pages/orchestration-notifications/Notifications.jsx
+++ src/scripts/modules/orchestrations/react/pages/orchestration-notifications/Notifications.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import { Map } from 'immutable';
-import SubscribersList from './SubscribersList';
 import Select from 'react-select';
-import { FormControl } from 'react-bootstrap';
+import {FormControl} from 'react-bootstrap';
+import RoutesStore from '../../../../../stores/RoutesStore';
+import OrchestrationStore from '../../../stores/OrchestrationsStore';
+import OrchestrationActionCreators from '../../../ActionCreators';
+import createStoreMixin from '../../../../../react/mixins/createStoreMixin';
+import EditButtons from '../../../../../react/common/EditButtons';
+
 
 export default React.createClass({
-  propTypes: {
-    notifications: React.PropTypes.object.isRequired, // notifications in structure from API
-    isEditing: React.PropTypes.bool.isRequired,
-    onNotificationsChange: React.PropTypes.func.isRequired
+  mixins: [createStoreMixin(OrchestrationStore)],
+
+  // componentWillReceiveProps() {
+  //   return this.setState(this.getStateFromStores());
+  // },
+
+  getStateFromStores() {
+    let notifications;
+    const orchestrationId = RoutesStore.getCurrentRouteIntParam('orchestrationId');
+    const orchestration = OrchestrationStore.get(orchestrationId);
+    const isEditing = OrchestrationStore.isEditing(orchestrationId, 'notifications');
+
+    if (isEditing) {
+      notifications = OrchestrationStore.getEditingValue(orchestrationId, 'notifications');
+    } else {
+      notifications = orchestration.get('notifications');
+    }
+
+    return {
+      orchestrationId,
+      orchestration,
+      notifications,
+      filter: OrchestrationStore.getFilter(),
+      isEditing: OrchestrationStore.isEditing(orchestrationId, 'notifications'),
+      isSaving: OrchestrationStore.isSaving(orchestrationId, 'notifications'),
+      filteredOrchestrations: OrchestrationStore.getFiltered()
+    };
   },
+
 
   render() {
     const errorEmails = this._getNotificationsForChannel('error');
@@ -18,6 +47,14 @@ export default React.createClass({
 
     return (
       <div className="kbc-block-with-padding">
+        <EditButtons
+          isEditing={this.state.isEditing}
+          isSaving={this.state.isSaving}
+          editLabel="Edit Notifications"
+          onCancel={this._handleCancel}
+          onSave={this._handleSave}
+          onEditStart={this._handleStart}
+        />
         <div>
           <p>
             {'Subscribe to receive notifications on some of the orchestration job '}
@@ -27,38 +64,15 @@ export default React.createClass({
         <div>
           <h2>Errors</h2>
           <p>Get notified when the orchestration finishes with an error.</p>
-          {this.props.isEditing ? (
-            this._renderNotificationsEditor('error', errorEmails)
-          ) : (
-            <SubscribersList emails={errorEmails} />
-          )}
+          {this._renderNotificationsEditor('error', errorEmails)}
           <h2>Warnings</h2>
           <p>Get notified when the orchestration finishes with a warning.</p>
-          {this.props.isEditing ? (
-            this._renderNotificationsEditor('warning', warningEmails)
-          ) : (
-            <SubscribersList emails={warningEmails} />
-          )}
+          {this._renderNotificationsEditor('warning', warningEmails)}
         </div>
         <div>
           <h2>Processing</h2>
-          {this.props.isEditing && processingEmails.count() ? (
-            <p>
-              {'Get notified when a job is processing '}
-              {this._renderToleranceInput()}
-              {' % longer than usual.'}
-            </p>
-          ) : (
-            <p>
-              {'Get notified when a job is processing '}
-              {this._getTolerance()}% longer than usual.
-            </p>
-          )}
-          {this.props.isEditing ? (
-            this._renderNotificationsEditor('processing', processingEmails)
-          ) : (
-            <SubscribersList emails={processingEmails} />
-          )}
+          <p>Get notified when a job is processing {this._renderToleranceInput()} % longer than usual.</p>
+          {this._renderNotificationsEditor('processing', processingEmails)}
         </div>
       </div>
     );
@@ -112,23 +126,23 @@ export default React.createClass({
       })
     );
 
-    newNotifications = this.props.notifications
+    newNotifications = this.state.notifications
       .filter(notification => notification.get('channel') !== channelName)
       .concat(newNotifications);
 
-    return this.props.onNotificationsChange(newNotifications);
+    return this._handleNotificationsChange(newNotifications);
   },
 
   _onToleranceChange(e) {
     const tolerance = parseInt(e.target.value, 10);
-    const newNotifications = this.props.notifications.map(notification => {
+    const newNotifications = this.state.notifications.map(notification => {
       if (notification.get('channel') !== 'processing') {
         return notification;
       } else {
         return notification.setIn(['parameters', 'tolerance'], tolerance);
       }
     });
-    return this.props.onNotificationsChange(newNotifications);
+    return this._handleNotificationsChange(newNotifications);
   },
 
   _getTolerance() {
@@ -144,6 +158,30 @@ export default React.createClass({
   },
 
   _getNotificationsForChannel(channelName) {
-    return this.props.notifications.filter(notification => notification.get('channel') === channelName);
+    return this.state.notifications.filter(notification => notification.get('channel') === channelName);
+  },
+
+  _handleFilterChange(query) {
+    return OrchestrationActionCreators.setOrchestrationsFilter(query);
+  },
+
+  _handleNotificationsChange(newNotifications) {
+    return OrchestrationActionCreators.updateOrchestrationNotificationsEdit(
+      this.state.orchestrationId,
+      newNotifications
+    );
+  },
+
+  _handleSave() {
+    return OrchestrationActionCreators.saveOrchestrationNotificationsEdit(this.state.orchestrationId);
+  },
+
+  _handleCancel() {
+    return OrchestrationActionCreators.cancelOrchestrationNotificationsEdit(this.state.orchestrationId);
+  },
+
+  _handleStart() {
+    return OrchestrationActionCreators.startOrchestrationNotificationsEdit(this.state.orchestrationId);
   }
+
 });
