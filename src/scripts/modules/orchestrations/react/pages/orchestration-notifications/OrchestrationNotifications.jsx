23be6585230e5677f2ae23eac63a9c8e4ed38ee6
janmichek <jan@keboola.com>
2018-11-29 15:55:34 +0100

    changes template routing

--- src/scripts/modules/orchestrations/react/pages/orchestration-notifications/OrchestrationNotifications.jsx
+++ src/scripts/modules/orchestrations/react/pages/orchestration-notifications/OrchestrationNotifications.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-
-// actions and stores
-import createStoreMixin from '../../../../../react/mixins/createStoreMixin';
-import OrchestrationsActionCreators from '../../../ActionCreators';
-import OrchestrationStore from '../../../stores/OrchestrationsStore';
+import { Map } from 'immutable';
+import Select from 'react-select';
+import { FormControl} from 'react-bootstrap';
 import RoutesStore from '../../../../../stores/RoutesStore';
-
-// React components
-import Notifications from './Notifications';
+import OrchestrationStore from '../../../stores/OrchestrationsStore';
+import OrchestrationActionCreators from '../../../ActionCreators';
+import createStoreMixin from '../../../../../react/mixins/createStoreMixin';
+import ConfirmButtons from '../../../../../react/common/ConfirmButtons';
 
 export default React.createClass({
   mixins: [createStoreMixin(OrchestrationStore)],
@@ -25,41 +24,164 @@ export default React.createClass({
     }
 
     return {
+      orchestrationId,
       orchestration,
       notifications,
       filter: OrchestrationStore.getFilter(),
-      isEditing,
+      isEditing: OrchestrationStore.isEditing(orchestrationId, 'notifications'),
       isSaving: OrchestrationStore.isSaving(orchestrationId, 'notifications'),
       filteredOrchestrations: OrchestrationStore.getFiltered()
     };
   },
 
-  componentWillReceiveProps() {
-    return this.setState(this.getStateFromStores());
+  render() {
+    const errorEmails = this._getNotificationsForChannel('error');
+    const warningEmails = this._getNotificationsForChannel('warning');
+    const processingEmails = this._getNotificationsForChannel('processing');
+
+    return (
+      <div className="container-fluid">
+        <div className="kbc-main-content">
+          <div className="kbc-block-with-padding">
+            <p>
+              <ConfirmButtons
+                isSaving={this.state.isSaving}
+                onCancel={this._handleCancel}
+                onSave={this._handleSave}
+                isDisabled={!this.state.isEditing}
+                className="text-right"
+              />
+            </p>
+            <p>
+              {'Subscribe to receive notifications on some of the orchestration job '}
+              events that might require your attention.
+            </p>
+
+            <h2>Errors</h2>
+            <p>Get notified when the orchestration finishes with an error.</p>
+            {this._renderNotificationsEditor('error', errorEmails)}
+
+            <h2>Warnings</h2>
+            <p>Get notified when the orchestration finishes with a warning.</p>
+            {this._renderNotificationsEditor('warning', warningEmails)}
+
+            <h2>Processing</h2>
+            <p>Get notified when a job is processing {this._renderToleranceInput()} % longer than usual.</p>
+            {this._renderNotificationsEditor('processing', processingEmails)}
+          </div>
+        </div>
+      </div>
+    );
+  },
+
+  _renderToleranceInput() {
+    const notificationsCount = this._getNotificationsForChannel('processing').filter(notification =>
+      notification.hasIn(['parameters', 'tolerance'])
+    ).count();
+    return (
+      <FormControl
+        type="number"
+        value={this._getTolerance()}
+        onChange={this._onToleranceChange}
+        disabled={!notificationsCount}
+        style={{
+          width: '80px',
+          display: 'inline-block'
+        }}
+      />
+    );
+  },
+
+  _renderNotificationsEditor(channelName, emails) {
+    return (
+      <Select.Creatable
+        multi={true}
+        backspaceRemoves={false}
+        deleteRemoves={false}
+        value={this._enteredEmails(emails)}
+        noResultsText=""
+        placeholder="Enter email"
+        promptTextCreator={() => 'Add email'}
+        onChange={newEmails => this._onChannelChange(channelName, newEmails)}
+      />
+    );
+  },
+
+  _enteredEmails(emails) {
+    return emails
+      .map(email => ({
+        value: email.get('email'),
+        label: email.get('email')
+      }))
+      .toArray();
+  },
+
+  _onChannelChange(channelName, newEmails) {
+    const tolerance = this._getTolerance();
+    let newNotifications = newEmails.map(email =>
+      Map({
+        email: email.value,
+        channel: channelName,
+        parameters: Map(channelName === 'processing' ? { tolerance } : {})
+      })
+    );
+
+    newNotifications = this.state.notifications
+      .filter(notification => notification.get('channel') !== channelName)
+      .concat(newNotifications);
+
+    return this._handleNotificationsChange(newNotifications);
+  },
+
+  _onToleranceChange(e) {
+    const tolerance = parseInt(e.target.value, 10);
+    const newNotifications = this.state.notifications.map(notification => {
+      if (notification.get('channel') !== 'processing') {
+        return notification;
+      } else {
+        return notification.setIn(['parameters', 'tolerance'], tolerance);
+      }
+    });
+    return this._handleNotificationsChange(newNotifications);
+  },
+
+  _getTolerance() {
+    const notifications = this._getNotificationsForChannel('processing').filter(notification =>
+      notification.hasIn(['parameters', 'tolerance'])
+    );
+
+    if (!notifications.count()) {
+      return 20;
+    } else {
+      return notifications.first().getIn(['parameters', 'tolerance']);
+    }
+  },
+
+  _getNotificationsForChannel(channelName) {
+    return this.state.notifications.filter(notification => notification.get('channel') === channelName);
   },
 
   _handleFilterChange(query) {
-    return OrchestrationsActionCreators.setOrchestrationsFilter(query);
+    return OrchestrationActionCreators.setOrchestrationsFilter(query);
   },
 
   _handleNotificationsChange(newNotifications) {
-    return OrchestrationsActionCreators.updateOrchestrationNotificationsEdit(
-      this.state.orchestration.get('id'),
+    return OrchestrationActionCreators.updateOrchestrationNotificationsEdit(
+      this.state.orchestrationId,
       newNotifications
     );
   },
 
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="kbc-main-content">
-          <Notifications
-            notifications={this.state.notifications}
-            isEditing={this.state.isEditing}
-            onNotificationsChange={this._handleNotificationsChange}
-          />
-        </div>
-      </div>
-    );
+  _handleSave() {
+    return OrchestrationActionCreators.saveOrchestrationNotificationsEdit(this.state.orchestrationId);
+  },
+
+  _handleCancel() {
+    return OrchestrationActionCreators.cancelOrchestrationNotificationsEdit(this.state.orchestrationId);
+  },
+
+  _handleStart() {
+    return !!OrchestrationActionCreators.startOrchestrationNotificationsEdit(this.state.orchestrationId).size;
   }
+
 });
