2e534ba285dd93695ae32aa6099cc570eb2ce155
Vojta Tůma <ja@vojta-tuma.cz>
2018-11-29 13:44:25 +0100

    Better test for SourceServerAdapter

--- src/scripts/modules/ex-ftp/adapters/SourceServer.spec.def.js
+++ src/scripts/modules/ex-ftp/adapters/SourceServer.spec.def.js
@@ -38,6 +38,126 @@ export const cases = {
         'username': 'dave'
       }
     }
+  },
+  'connectionType': {
+    'localState': {
+      'connectionType': 'FTPS',
+      'host': '',
+      'password': '',
+      'port': 21,
+      'privateKey': '',
+      'username': ''
+    },
+    'configuration': {
+      'parameters': {
+        '#password': '',
+        '#privateKey': '',
+        'connectionType': 'FTPS',
+        'host': '',
+        'port': 21,
+        'username': ''
+      }
+    }
+  },
+  'password': {
+    'localState': {
+      'connectionType': '',
+      'host': '',
+      'password': 'pass',
+      'port': 21,
+      'privateKey': '',
+      'username': ''
+    },
+    'configuration': {
+      'parameters': {
+        '#password': 'pass',
+        '#privateKey': '',
+        'connectionType': '',
+        'host': '',
+        'port': 21,
+        'username': ''
+      }
+    }
+  },
+  'port': {
+    'localState': {
+      'connectionType': '',
+      'host': '',
+      'password': 'pass',
+      'port': 22,
+      'privateKey': '',
+      'username': ''
+    },
+    'configuration': {
+      'parameters': {
+        '#password': 'pass',
+        '#privateKey': '',
+        'connectionType': '',
+        'host': '',
+        'port': 22,
+        'username': ''
+      }
+    }
+  },
+  'username': {
+    'localState': {
+      'connectionType': '',
+      'host': '',
+      'password': '',
+      'port': 21,
+      'privateKey': '',
+      'username': 'name'
+    },
+    'configuration': {
+      'parameters': {
+        '#password': '',
+        '#privateKey': '',
+        'connectionType': '',
+        'host': '',
+        'port': 21,
+        'username': 'name'
+      }
+    }
+  },
+  'privateKey': {
+    'localState': {
+      'connectionType': '',
+      'host': '',
+      'password': '',
+      'port': 21,
+      'privateKey': 'pks#123013010230',
+      'username': ''
+    },
+    'configuration': {
+      'parameters': {
+        '#password': '',
+        '#privateKey': 'pks#123013010230',
+        'connectionType': '',
+        'host': '',
+        'port': 21,
+        'username': ''
+      }
+    }
+  },
+  'host': {
+    'localState': {
+      'connectionType': '',
+      'host': 'abcd.com',
+      'password': '',
+      'port': 21,
+      'privateKey': 'pks#123013010230',
+      'username': ''
+    },
+    'configuration': {
+      'parameters': {
+        '#password': '',
+        '#privateKey': 'pks#123013010230',
+        'connectionType': '',
+        'host': 'abcd.com',
+        'port': 21,
+        'username': ''
+      }
+    }
   }
 };
 
