c62aaa0227f87fa46b1d79925a20a49300888a4e
Vladimír Kriška <vlado@keboola.com>
2018-12-04 15:16:38 +0100

    Use find instead of filter+get

--- src/scripts/modules/transformations/react/components/mapping/InputMappingRowSnowflakeEditorHelper.js
+++ src/scripts/modules/transformations/react/components/mapping/InputMappingRowSnowflakeEditorHelper.js
@@ -3,23 +3,20 @@ import { SnowflakeDataTypesMapping } from '../../../Constants';
 
 const getMetadataDataTypes = (columnMetadata) => {
   return columnMetadata.map((metadata, colname) => {
-    const baseTypes = metadata.filter((entry) => {
+    const baseType = metadata.find((entry) => {
       return entry.get('key') === 'KBC.datatype.basetype';
     });
-    if (baseTypes.count() === 0) {
+    if (!baseType) {
       return null;
     }
-    const baseType = baseTypes.get(0);
 
-    const dataTypeLengths = metadata.filter((entry) => {
+    const dataTypeLength = metadata.find((entry) => {
       return entry.get('key') === 'KBC.datatype.length';
-    });
-    const dataTypeLength = dataTypeLengths.count() > 0 ? dataTypeLengths.get(0) : Map();
+    }, null, Map());
 
-    const dataTypeNullables = metadata.filter((entry) => {
+    const dataTypeNullable = metadata.find((entry) => {
       return entry.get('key') === 'KBC.datatype.nullable';
-    });
-    const dataTypeNullable = dataTypeNullables.count() > 0 ? dataTypeNullables.get(0) : Map();
+    }, null, Map());
 
     const matchedDataType = SnowflakeDataTypesMapping.find((mappedDatatype) => {
       return mappedDatatype.get('basetype') === baseType.get('value');
