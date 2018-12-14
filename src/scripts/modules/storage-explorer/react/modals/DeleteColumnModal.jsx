07adde4b4926ff1f151d589ec4d2b672a9809232
Jakub Kotek <blaztar@gmail.com>
2018-12-14 08:43:18 +0100

    Delete column modal - use ConfirmButtons, Form

--- src/scripts/modules/storage-explorer/react/modals/DeleteColumnModal.jsx
+++ src/scripts/modules/storage-explorer/react/modals/DeleteColumnModal.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import { Modal, ButtonToolbar, Button, Checkbox, Table } from 'react-bootstrap';
+import { Form, Modal, Checkbox, Table } from 'react-bootstrap';
+import ConfirmButtons from '../../../../react/common/ConfirmButtons';
 
 const INITIAL_STATE = {
   forceDelete: false
@@ -23,94 +24,97 @@ export default React.createClass({
   render() {
     return (
       <Modal onHide={this.onHide} show={true}>
-        <Modal.Header closeButton>
-          <Modal.Title>Delete column</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            Do you really want to delete column <b>{this.props.column}</b>?
-          </p>
+        <Form onSubmit={this.handleSubmit}>
+          <Modal.Header closeButton>
+            <Modal.Title>Delete column</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Do you really want to delete column <b>{this.props.column}</b>?
+            </p>
 
-          {this.hasAliasesOrLinks() && (
-            <div>
-              <Checkbox checked={this.state.forceDelete} onChange={this.toggleForceDelete}>
-                Delete column from table and table aliases
-              </Checkbox>
+            {this.hasAliasesOrLinks() && (
+              <div>
+                <Checkbox checked={this.state.forceDelete} onChange={this.toggleForceDelete}>
+                  Delete column from table and table aliases
+                </Checkbox>
 
-              <Table>
-                <thead>
-                  <tr>
-                    <th>Table aliases</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.props.tableAliases.map(alias => {
-                    return (
-                      <tr key={alias.get('id')}>
-                        <td>
-                          <Link
-                            to="storage-explorer-table"
-                            params={{ bucketId: alias.getIn(['bucket', 'id']), tableName: alias.get('name') }}
-                            onClick={this.onHide}
-                          >
-                            {alias.get('id')}
-                          </Link>
-                        </td>
-                      </tr>
-                    );
-                  })}
-
-                  {this.props.tableLinks.map(alias => {
-                    const ownerId = this.props.sapiToken.getIn(['owner', 'id']);
-                    const isOrganizationMember = this.props.sapiToken.getIn(['admin', 'isOrganizationMember']);
-                    const project = alias.get('project');
-
-                    return (
-                      <tr key={alias.get('id')}>
-                        <td>
-                          {ownerId === project.get('id') && (
+                <Table>
+                  <thead>
+                    <tr>
+                      <th>Table aliases</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.props.tableAliases.map(alias => {
+                      return (
+                        <tr key={alias.get('id')}>
+                          <td>
                             <Link
                               to="storage-explorer-table"
-                              params={{ bucketId: alias.get('bucketId'), tableName: alias.get('tableName') }}
+                              params={{ bucketId: alias.getIn(['bucket', 'id']), tableName: alias.get('name') }}
                               onClick={this.onHide}
                             >
                               {alias.get('id')}
                             </Link>
-                          )}
+                          </td>
+                        </tr>
+                      );
+                    })}
 
-                          {ownerId !== project.get('id') && isOrganizationMember && (
-                            <span>
-                              {project.get('name')} / {alias.get('id')}
-                            </span>
-                          )}
+                    {this.props.tableLinks.map(alias => {
+                      const ownerId = this.props.sapiToken.getIn(['owner', 'id']);
+                      const isOrganizationMember = this.props.sapiToken.getIn(['admin', 'isOrganizationMember']);
+                      const project = alias.get('project');
 
-                          {ownerId !== project.get('id') &&
-                            isOrganizationMember &&
-                            <a href={`/admin/projects/${project.get('id')}`}>{project.get('name')}</a> /
-                            (
-                              <a href={`/admin/projects/${project.get('id')}/storage#/buckets/${alias.get('id')}`}>
+                      return (
+                        <tr key={alias.get('id')}>
+                          <td>
+                            {ownerId === project.get('id') && (
+                              <Link
+                                to="storage-explorer-table"
+                                params={{ bucketId: alias.get('bucketId'), tableName: alias.get('tableName') }}
+                                onClick={this.onHide}
+                              >
                                 {alias.get('id')}
-                              </a>
+                              </Link>
                             )}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </Table>
-            </div>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          <ButtonToolbar>
-            <Button onClick={this.onHide} bsStyle="link">
-              Cancel
-            </Button>
-            <Button onClick={this.handleConfirm} disabled={this.isDisable()} bsStyle="danger">
-              Delete
-            </Button>
-          </ButtonToolbar>
-        </Modal.Footer>
+
+                            {ownerId !== project.get('id') && isOrganizationMember && (
+                              <span>
+                                {project.get('name')} / {alias.get('id')}
+                              </span>
+                            )}
+
+                            {ownerId !== project.get('id') &&
+                              isOrganizationMember &&
+                              <a href={`/admin/projects/${project.get('id')}`}>{project.get('name')}</a> /
+                              (
+                                <a href={`/admin/projects/${project.get('id')}/storage#/buckets/${alias.get('id')}`}>
+                                  {alias.get('id')}
+                                </a>
+                              )}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              </div>
+            )}
+          </Modal.Body>
+          <Modal.Footer>
+            <ConfirmButtons
+              isSaving={false}
+              isDisabled={this.isDisabled()}
+              onSave={this.handleSubmit}
+              onCancel={this.onHide}
+              saveLabel="Delete"
+              saveStyle="danger"
+              saveButtonType="submit"
+            />
+          </Modal.Footer>
+        </Form>
       </Modal>
     );
   },
@@ -126,12 +130,13 @@ export default React.createClass({
     this.props.onHide();
   },
 
-  handleConfirm() {
+  handleSubmit(event) {
+    event.preventDefault();
     this.props.onConfirm(this.props.column, this.state.forceDelete);
     this.onHide();
   },
 
-  isDisable() {
+  isDisabled() {
     if (this.hasAliasesOrLinks() && !this.state.forceDelete) {
       return true;
     }
