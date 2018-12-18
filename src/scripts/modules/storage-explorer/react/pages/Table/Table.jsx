6dbdc0a8c8b7a62ef7519ba0280154fcd7a18be2
Jakub Kotek <blaztar@gmail.com>
2018-12-18 15:38:30 +0100

    add key to TableDetail

--- src/scripts/modules/storage-explorer/react/pages/Table/Table.jsx
+++ src/scripts/modules/storage-explorer/react/pages/Table/Table.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 import Buckets from '../../components/Buckets';
+import createStoreMixin from '../../../../../react/mixins/createStoreMixin';
+import RoutesStore from '../../../../../stores/RoutesStore';
 import TableDetail from './TableDetail';
 
 export default React.createClass({
+  mixins: [createStoreMixin(RoutesStore)],
+
+  getStateFromStores() {
+    return {
+      bucketId: RoutesStore.getCurrentRouteParam('bucketId'),
+      tableName: RoutesStore.getCurrentRouteParam('tableName')
+    };
+  },
+
   render() {
     return (
       <div className="container-fluid">
@@ -11,7 +22,7 @@ export default React.createClass({
           <Buckets />
         </Col>
         <Col sm={9} className="kbc-main-content">
-          <TableDetail />
+          <TableDetail key={`${this.state.bucketId}-${this.state.tableName}`} />
         </Col>
       </div>
     );
