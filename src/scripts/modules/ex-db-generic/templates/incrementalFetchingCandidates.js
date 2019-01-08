1f27b3296235002253c50c9d41e0426304f857ea
pivnicek <marc@keboola.com>
2019-01-08 10:34:11 +0100

    explicitly specify valid types for incremental fetching

--- src/scripts/modules/ex-db-generic/templates/incrementalFetchingCandidates.js
+++ src/scripts/modules/ex-db-generic/templates/incrementalFetchingCandidates.js
@@ -1,11 +1,18 @@
 import Immutable from 'immutable';
 
-export const incrementalFetchingTimestampTypes = Immutable.Map({
-  'keboola.ex-db-mysql': Immutable.List(['timestamp', 'datetime']),
-  'keboola.ex-db-mssql': Immutable.List(['datetime', 'datetime2', 'smalldatetime'])
-});
-
-export const incrementalFetchingAutoIncrementProperty = Immutable.Map({
-  'keboola.ex-db-mysql': 'autoIncrement',
-  'keboola.ex-db-mssql': 'autoIncrement'
+export const incrementalFetchingTypes = Immutable.Map({
+  'keboola.ex-db-mysql': Immutable.List(
+    [
+      'timestamp', 'datetime',
+      'integer', 'int', 'smallint', 'mediumint', 'bigint',
+      'decimal', 'numeric', 'float', 'double'
+    ]
+  ),
+  'keboola.ex-db-mssql': Immutable.List(
+    [
+      'datetime', 'datetime2', 'smalldatetime',
+      'integer', 'int', 'smallint', 'bigint',
+      'numeric', 'decimal', 'real', 'float'
+    ]
+  )
 });
