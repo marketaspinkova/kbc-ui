7bae43679f7f18ac061271fedf6d9afd7a9f7e15
Jakub Kotek <blaztar@gmail.com>
2019-01-08 09:49:50 +0100

    update New dimension form modal

--- src/scripts/modules/gooddata-writer-v3/react/components/NewDimensionForm.jsx
+++ src/scripts/modules/gooddata-writer-v3/react/components/NewDimensionForm.jsx
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import {Radio, HelpBlock, Form, Col, Checkbox, FormControl, FormGroup, ControlLabel} from 'react-bootstrap';
+import {PanelWithDetails} from '@keboola/indigo-ui';
 
 export default React.createClass({
   propTypes: {
@@ -29,48 +30,54 @@ export default React.createClass({
           </Col>
         </FormGroup>
         <FormGroup>
-          <Col componentClass={ControlLabel} sm={3}>Include Time</Col>
-          <Col sm={9}>
+          <Col sm={9} smOffset={3}>
             <Checkbox
               checked={value.includeTime}
               onChange={() => this.handleChange({includeTime: !value.includeTime})}>
-              Include
+              Include Time
             </Checkbox>
-          </Col>
-        </FormGroup>
-        <FormGroup>
-          <Col componentClass={ControlLabel} sm={3}>Identifier</Col>
-          <Col sm={9}>
-            <FormControl
-              type="text"
-              placeholder="optional"
-              disabled={disabled}
-              onChange={e => this.handleChange({identifier: e.target.value})}
-              value={value.identifier}
-            />
           </Col>
         </FormGroup>
         <FormGroup>
           <Col componentClass={ControlLabel} sm={3}>Template</Col>
           <Col sm={9}>
             {this.renderRadio('GoodData', 'gooddata', 'Default date dimension provided by GoodData')}
-            {this.renderRadio('Keboola', 'keboola', 'Default date dimension provided by Keboola. Added all week setups: Mon-Sun, Tue-Mon, Wed-Tue, Thu-Wed, Fri-Thu, Sat0Fri, Sun-Sat + Boolean value whether its weekend or working day')}
-            {this.renderRadio('Custom', 'custom', 'Provide your own template. You can generate the csv file containing all necessary details and provide it ti Goog Data.')}
+            {this.renderRadio('Keboola', 'keboola', 'Default date dimension provided by Keboola. Added all week setups: Mon-Sun, Tue-Mon, Wed-Tue, Thu-Wed, Fri-Thu, Sat-Fri, Sun-Sat + Boolean value whether its weekend or working day')}
+            {this.renderRadio('Custom', 'custom', 'Provide your own template. You can generate the csv file containing all necessary details and provide it to GoodData.')}
           </Col>
         </FormGroup>
-        {value.template !== 'keboola' && value.template !== 'gooddata' &&
-         <FormGroup>
-           <Col componentClass={ControlLabel} sm={3}>Template ID</Col>
-           <Col sm={9}>
-             <FormControl
-               type="text"
-               disabled={disabled}
-               onChange={e => this.handleChange({templateId: e.target.value})}
-               value={value.templateId}
-             />
-           </Col>
-         </FormGroup>
-        }
+        {value.template !== 'keboola' && value.template !== 'gooddata' && (
+          <FormGroup>
+            <Col componentClass={ControlLabel} sm={3}>Template ID</Col>
+            <Col sm={9}>
+              <FormControl
+                type="text"
+                disabled={disabled}
+                onChange={e => this.handleChange({templateId: e.target.value})}
+                value={value.templateId}
+              />
+            </Col>
+          </FormGroup>
+        )}
+        <PanelWithDetails
+          defaultExpanded={false}
+          labelCollapse="Hide Advanced Options"
+          labelOpen="Show Advanced Options"
+          placement="top"
+        >
+          <FormGroup>
+            <Col componentClass={ControlLabel} sm={3}>Identifier</Col>
+            <Col sm={9}>
+              <FormControl
+                type="text"
+                placeholder="optional"
+                disabled={disabled}
+                onChange={e => this.handleChange({identifier: e.target.value})}
+                value={value.identifier}
+              />
+            </Col>
+          </FormGroup>
+        </PanelWithDetails>
       </Form>
     );
   },
@@ -82,11 +89,12 @@ export default React.createClass({
           disabled={this.props.disabled}
           checked={this.props.value.template === value}
           onChange={() => this.handleChange({template: value})}
-          value={value} name="template" > {label} </Radio>
+          value={value} name="template"
+        >
+          {label}
+        </Radio>
         {helpText && <HelpBlock>{helpText}</HelpBlock>}
       </div>
     );
   }
-
-
 });
