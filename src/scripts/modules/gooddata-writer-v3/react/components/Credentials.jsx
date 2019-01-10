a8e0b1c28308383fadc21413f35b7b68eacb6e22
Jakub Kotek <blaztar@gmail.com>
2019-01-10 15:27:48 +0100

    Revert "remove disconnect button"
    This reverts commit 30a6f8fbb612f7b543c467142d68581d13a47ac2.

--- src/scripts/modules/gooddata-writer-v3/react/components/Credentials.jsx
+++ src/scripts/modules/gooddata-writer-v3/react/components/Credentials.jsx
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import {Loader, ExternalLink} from '@keboola/indigo-ui';
+import ResetProjectModal from './ResetProjectModal';
 import CreateProjectModal from './CreateProjectModal';
 import ComponentsStore from '../../../components/stores/ComponentsStore';
 
@@ -19,12 +20,14 @@ export default React.createClass({
     }),
     disabled: PropTypes.bool.isRequired,
     onHandleCreate: PropTypes.func.isRequired,
-    onToggleEnableAcess: PropTypes.func.isRequired
+    onToggleEnableAcess: PropTypes.func.isRequired,
+    onHandleResetProject: PropTypes.func.isRequired
   },
 
   getInitialState() {
     return {
-      showCreateProjectModal: false
+      showCreateProjectModal: false,
+      showResetProjectModal: false
     };
   },
 
@@ -38,9 +41,28 @@ export default React.createClass({
     this.props.onHandleCreate(newProject).then(this.closeCreateProjectModal);
   },
 
+
+  closeResetProjectModal() {
+    if (!this.props.disabled && !this.props.provisioning.isDeleting) {
+      this.setState({showResetProjectModal: false});
+    }
+  },
+
+  handleResetProject(deleteProject) {
+    return this.props.onHandleResetProject(deleteProject).then(this.closeResetProjectModal);
+  },
+
   render() {
     return (
       <div>
+        <ResetProjectModal
+          isReseting={this.props.provisioning.isDeleting}
+          show={this.state.showResetProjectModal}
+          pid={this.props.config.pid}
+          onHide={() => this.setState({showResetProjectModal: false})}
+          onConfirm={this.handleResetProject}
+          disabled={this.props.disabled}
+        />
         <CreateProjectModal
           isCreating={this.props.provisioning.isCreating}
           show={this.state.showCreateProjectModal}
@@ -122,6 +144,7 @@ export default React.createClass({
         }
         {!isProvisioned && this.renderGoToProjectLink()}
 
+        {isProvisioned && this.renderResetProjectButton()}
         {isProvisioned && !hasSso && this.renderEnableAccessButton()}
         {isProvisioned &&  hasSso && this.renderDisableAccessButton()}
         {isProvisioned &&  hasSso && this.renderLoginToProjectLink()}
@@ -140,6 +163,17 @@ export default React.createClass({
 
   isSsoEnabled() {
     return this.isKeboolaProvisioned() && !!this.props.provisioning.data.get('sso');
+  },
+
+
+  renderResetProjectButton() {
+    return (
+      <button type="button"
+        onClick={() => this.setState({showResetProjectModal: true})}
+        className="btn btn-danger pull-right">
+        Disconnect
+      </button>
+    );
   },
 
   renderGoToProjectLink() {
