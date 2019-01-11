7344a6da869e6591d2ef8addb6de99d7a8c06086
Jakub Kotek <blaztar@gmail.com>
2019-01-11 08:47:59 +0100

    basis top nav

--- src/scripts/modules/storage-explorer/Routes.js
+++ src/scripts/modules/storage-explorer/Routes.js
@@ -3,6 +3,7 @@ import Files from './react/pages/Files/Files';
 import Jobs from './react/pages/Jobs/Jobs';
 import Table from './react/pages/Table/Table';
 import Bucket from './react/pages/Bucket/Bucket';
+import NavButtons from './react/components/NavButtons';
 import FilesReloaderButton from './react/components/FilesReloaderButton';
 import JobsReloaderButton from './react/components/JobsReloaderButton';
 import StorageActions from '../components/StorageActionCreators';
@@ -14,12 +15,14 @@ export default {
   name: 'storage-explorer',
   title: 'Storage Explorer',
   defaultRouteHandler: Index,
+  headerButtonsHandler: NavButtons,
   requireData: [() => StorageActions.loadBuckets(), () => StorageActions.loadTables()],
   childRoutes: [
     {
       name: 'storage-explorer-files',
       path: 'files',
       defaultRouteHandler: Files,
+      headerButtonsHandler: NavButtons,
       reloaderHandler: FilesReloaderButton,
       title: 'Files',
       requireData: [() => StorageActions.loadFilesForce({ limit: filesLimit })]
@@ -28,6 +31,7 @@ export default {
       name: 'storage-explorer-jobs',
       path: 'jobs',
       defaultRouteHandler: Jobs,
+      headerButtonsHandler: NavButtons,
       reloaderHandler: JobsReloaderButton,
       title: 'Jobs',
       requireData: [() => StorageActions.loadJobs({ limit: jobsLimit })]
@@ -36,6 +40,7 @@ export default {
       name: 'storage-explorer-bucket',
       path: ':bucketId',
       defaultRouteHandler: Bucket,
+      headerButtonsHandler: NavButtons,
       title(routerState) {
         return `Bucket ${routerState.getIn(['params', 'bucketId'])}`;
       },
@@ -44,6 +49,7 @@ export default {
           name: 'storage-explorer-table',
           path: ':tableName',
           defaultRouteHandler: Table,
+          headerButtonsHandler: NavButtons,
           title(routerState) {
             const bucketId = routerState.getIn(['params', 'bucketId']);
             const tableName = routerState.getIn(['params', 'tableName']);
---
+++ src/scripts/modules/storage-explorer/react/components/NavButtons.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import classnames from 'classnames';
+import { Link } from 'react-router';
+import createStoreMixin from '../../../../react/mixins/createStoreMixin';
+import RoutesStore from '../../../../stores/RoutesStore';
+
+export default React.createClass({
+  mixins: [createStoreMixin(RoutesStore)],
+
+  getStateFromStores() {
+    return {
+      routeName: RoutesStore.getCurrentRouteConfig().get('name')
+    };
+  },
+
+  render() {
+    return (
+      <ul className="nav nav-pills navbar-nav pull-right">
+        <li className={this.activeClass('buckets')}>
+          <Link to="storage-explorer">Buckets</Link>
+        </li>
+        <li className={this.activeClass('files')}>
+          <Link to="storage-explorer-files">Files</Link>
+        </li>
+        <li className={this.activeClass('jobs')}>
+          <Link to="storage-explorer-jobs">Jobs</Link>
+        </li>
+      </ul>
+    );
+  },
+
+  activeClass(name) {
+    return classnames({ active: this.activeLink() === name });
+  },
+
+  activeLink() {
+    if (this.state.routeName === 'storage-explorer-jobs') {
+      return 'jobs';
+    }
+
+    if (this.state.routeName === 'storage-explorer-files') {
+      return 'files';
+    }
+
+    return 'buckets';
+  }
+});
