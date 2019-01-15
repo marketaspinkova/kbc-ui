037189a8d4168ad960d6b270cc21533198b1b782
Vladimír Kriška <vlado@keboola.com>
2019-01-15 16:34:14 +0100

    Set warnings and longStackTraces by checking NODE_ENV

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -30,7 +30,9 @@ import ErrorNotification from './react/common/ErrorNotification';
 // Promise global config
 // Note: long stack traces and warnings are enabled in dev env by default
 Promise.config({
-  cancellation: true
+  cancellation: true,
+  longStackTraces: process.env.NODE_ENV === 'development',
+  warnings: process.env.NODE_ENV === 'development'
 });
 
 /*
