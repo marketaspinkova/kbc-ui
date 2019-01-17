2e0248655c9c52fcf6d607ccaa07e17a9abb934b
Jakub Kotek <blaztar@gmail.com>
2019-01-17 20:39:14 +0100

    update ValidateQueriesButton component

--- src/scripts/modules/transformations/react/components/ValidateQueriesButton.jsx
+++ src/scripts/modules/transformations/react/components/ValidateQueriesButton.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { Label } from 'react-bootstrap';
 import Modal from '../modals/ValidateQueriesModal';
 
 export default React.createClass({
   propTypes: {
-    transformationId: React.PropTypes.string.isRequired,
-    bucketId: React.PropTypes.string.isRequired,
-    backend: React.PropTypes.string.isRequired,
-    modalOpen: React.PropTypes.bool.isRequired,
-    onModalOpen: React.PropTypes.func.isRequired,
-    onModalClose: React.PropTypes.func.isRequired,
-    isSaved: React.PropTypes.bool.isRequired
+    transformationId: PropTypes.string.isRequired,
+    bucketId: PropTypes.string.isRequired,
+    backend: PropTypes.string.isRequired,
+    modalOpen: PropTypes.bool.isRequired,
+    onModalOpen: PropTypes.func.isRequired,
+    onModalClose: PropTypes.func.isRequired,
+    isSaved: PropTypes.bool.isRequired
   },
 
   handleOpenButtonClick(e) {
     e.preventDefault();
-    return this.props.onModalOpen();
+    this.props.onModalOpen();
   },
 
   render() {
     return (
       <a onClick={this.handleOpenButtonClick}>
-        <i className="fa fa-check-square-o fa-fw" />
-        {' '}Validate
-        <span>{' '}
-          <span className="label label-info">BETA</span>
-        </span>
+        <i className="fa fa-check-square-o fa-fw" /> Validate <Label bsStyle="info">BETA</Label>
         <Modal
           transformationId={this.props.transformationId}
           bucketId={this.props.bucketId}
@@ -33,7 +30,6 @@ export default React.createClass({
           onHide={this.props.onModalClose}
           isSaved={this.props.isSaved}
         />
-
       </a>
     );
   }
