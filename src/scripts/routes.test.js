aa17a7067bff693e0a675dbbb9571783f84ab250
Vladimír Kriška <vlado@keboola.com>
2019-01-21 10:37:16 +0100

    Add test/snapshot for router configuration

---
+++ src/scripts/routes.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/no-multi-comp */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import routes from './routes';
+import createReactRouterRoutes from './utils/createReactRouterRoutes';
+
+jest.mock('react-router', () => {
+  const DefaultRoute = () => (<span />);
+  const NotFoundRoute = () => (<span />);
+  const RouteHandler = () => (<span />);
+  const Route = ({ children, ...props }) => (<span {...props}>{children}</span>);
+
+  return {
+    DefaultRoute,
+    NotFoundRoute,
+    RouteHandler,
+    Route
+  };
+});
+
+describe('routes config', function() {
+  it('should get router configuration', function() {
+    shallowSnapshot(createReactRouterRoutes(routes));
+  });
+});
