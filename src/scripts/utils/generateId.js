9d480f3dc1b60ab350f74a1b99dc83f5a98536fd
Vladimír Kriška <vlado@keboola.com>
2019-02-03 16:09:32 +0100

    Move randomNumber fn to separate module (easier testing)

--- src/scripts/utils/generateId.js
+++ src/scripts/utils/generateId.js
@@ -1,4 +1,4 @@
-export const randomNumber = (max = 100000) => Math.floor(Math.random() * max + 1);
+import randomNumber from './randomNumber';
 
 export default function(ids = []) {
   let newId = randomNumber();
