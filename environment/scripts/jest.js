ad4b5cc1510363beb05f1d65ef144aa211121446
Vladimír Kriška <vlado@keboola.com>
2019-02-05 15:25:34 +0100

    Remove jest --forceExit on CI

--- environment/scripts/jest.js
+++ environment/scripts/jest.js
@@ -4,11 +4,6 @@ process.env.TZ = 'UTC';
 const jest = require('jest');
 const argv = process.argv.slice(2);
 
-// NOTE: temporary fix for enable console.log on localhost.
-if (process.env.CI === 'true') {
-  argv.push('--forceExit');
-}
-
 if (process.env.DEV_CACHE !== 'true') {
   argv.push('--no-cache');
 }
