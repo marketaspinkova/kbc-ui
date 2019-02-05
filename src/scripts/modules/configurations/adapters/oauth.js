9b40c6cd19e874e75e10caef43d7f049cf563ef2
Miroslav Čillík <miro@keboola.com>
2019-02-05 12:22:07 +0100

    fix (configurations/adapter/oauth): supports version attribute

--- src/scripts/modules/configurations/adapters/oauth.js
+++ src/scripts/modules/configurations/adapters/oauth.js
@@ -1,18 +1,29 @@
 import Immutable from 'immutable';
 
+const OAUTH_FALLBACK_VERSION = 2;
+
+const OAUTH_V3 = 3;
+
 const createConfiguration = function(localState) {
-  return Immutable.fromJS({
+  const config = Immutable.fromJS({
     authorization: {
       oauth_api: {
         id: localState.get('oauthId', '')
       }
     }
   });
+
+  if (localState.has('oauthVersion') && localState.get('oauthVersion') === OAUTH_V3) {
+    return config.setIn(['authorization', 'oauth_api', 'version'], OAUTH_V3);
+  }
+
+  return config;
 };
 
 const parseConfiguration = function(configuration, context) {
   return Immutable.fromJS({
     oauthId: configuration.getIn(['authorization', 'oauth_api', 'id'], ''),
+    oauthVersion: configuration.getIn(['authorization', 'oauth_api', 'version'], OAUTH_FALLBACK_VERSION),
     componentId: context.get('componentId', ''),
     configurationId: context.get('configurationId', '')
   });
