e388aa626be572be8f7553f7a1d961752bebb53a
Jakub Kotek <blaztar@gmail.com>
2019-02-07 12:13:39 +0100

    fix routes expotr

--- src/scripts/modules/components/Routes.js
+++ src/scripts/modules/components/Routes.js
@@ -43,138 +43,144 @@ const extractor = injectProps({ type: 'extractor' });
 const writer = injectProps({ type: 'writer' });
 const application = injectProps({ type: 'application' });
 
-export default {
-  applications: {
-    name: 'applications',
-    title: 'Applications',
-    requireData: function() {
-      return InstalledComponentsActionsCreators.loadComponents();
-    },
-    defaultRouteHandler: application(ComponentsIndex),
-    headerButtonsHandler: injectProps({
-      addRoute: 'new-application',
-      type: 'application'
-    })(ComponentsHeaderButtons),
-    reloaderHandler: ComponentReloaderButton,
-    childRoutes: [
-      {
-        name: 'new-application',
-        title: 'New Application',
-        defaultRouteHandler: application(NewComponent)
-      },
-      createComponentRoute('geneea-nlp-analysis', [geneeaGeneralRoutes]),
-      createComponentRoute('geneea.nlp-analysis-v2', [geneeaV2Routes]),
-      createComponentRoute('custom-science', [customScienceRoutes]),
-      createComponentRoute('keboola.app-snowflake-dwh-manager', [appSnowflakeDwhManagerRoutes]),
-      createGenericDetailRoute('application')
-    ]
+const applications = {
+  name: 'applications',
+  title: 'Applications',
+  requireData: function() {
+    return InstalledComponentsActionsCreators.loadComponents();
   },
-  extractors: {
-    name: 'extractors',
-    title: 'Extractors',
-    requireData: function() {
-      return InstalledComponentsActionsCreators.loadComponents();
+  defaultRouteHandler: application(ComponentsIndex),
+  headerButtonsHandler: injectProps({
+    addRoute: 'new-application',
+    type: 'application'
+  })(ComponentsHeaderButtons),
+  reloaderHandler: ComponentReloaderButton,
+  childRoutes: [
+    {
+      name: 'new-application',
+      title: 'New Application',
+      defaultRouteHandler: application(NewComponent)
     },
-    defaultRouteHandler: extractor(ComponentsIndex),
-    headerButtonsHandler: injectProps({
-      addRoute: 'new-extractor',
-      type: 'extractor'
-    })(ComponentsHeaderButtons),
-    reloaderHandler: ComponentReloaderButton,
-    childRoutes: [
-      {
-        name: 'new-extractor',
-        title: 'New Extractor',
-        defaultRouteHandler: extractor(NewComponent)
-      },
-      createComponentRoute('keboola.ex-google-analytics-v5', [exGaV4Routes('keboola.ex-google-analytics-v5')]),
-      createComponentRoute('keboola.ex-google-analytics-v4', [exGaV4Routes('keboola.ex-google-analytics-v4')]),
-      createComponentRoute('keboola.ex-google-analytics', [exGaV4Routes('keboola.ex-google-analytics')]),
-      createComponentRoute('keboola.ex-facebook', [exFacebookRoutes('keboola.ex-facebook')]),
-      createComponentRoute('keboola.ex-facebook-ads', [exFacebookRoutes('keboola.ex-facebook-ads')]),
-      createComponentRoute('keboola.ex-instagram', [exFacebookRoutes('keboola.ex-instagram')]),
-      createComponentRoute('keboola.ex-google-drive', [exGdriveNewRoutes]),
-      createComponentRoute('ex-adform', [adformRoutes]),
-      createComponentRoute('keboola.ex-twitter', [twitterRoutes]),
-      createComponentRoute('ex-dropbox', [dropoxExtractorRoutes]),
-      createComponentRoute('radektomasek.ex-dropbox-v2', [dropoxExtractorRoutesV2]),
-      createComponentRoute('keboola.ex-db-pgsql', [exDbGenericRoutes('keboola.ex-db-pgsql')]),
-      createComponentRoute('keboola.ex-db-redshift', [exDbGenericRoutes('keboola.ex-db-redshift')]),
-      createComponentRoute('keboola.ex-db-redshift-cursors', [exDbGenericRoutes('keboola.ex-db-redshift-cursors')]),
-      createComponentRoute('keboola.ex-db-firebird', [exDbGenericRoutes('keboola.ex-db-firebird')]),
-      createComponentRoute('keboola.ex-db-db2', [exDbGenericRoutes('keboola.ex-db-db2')]),
-      createComponentRoute('keboola.ex-db-db2-bata', [exDbGenericRoutes('keboola.ex-db-db2-bata')]),
-      createComponentRoute('keboola.ex-db-mssql', [exDbGenericRoutes('keboola.ex-db-mssql')]),
-      createComponentRoute('keboola.ex-db-mysql', [exDbGenericRoutes('keboola.ex-db-mysql')]),
-      createComponentRoute('keboola.ex-db-mysql-custom', [exDbGenericRoutes('keboola.ex-db-mysql-custom')]),
-      createComponentRoute('keboola.ex-db-oracle', [exDbGenericRoutes('keboola.ex-db-oracle')]),
-      createComponentRoute('keboola.ex-db-snowflake', [exDbGenericRoutes('keboola.ex-db-snowflake')]),
-      createComponentRoute('keboola.ex-db-impala', [exDbGenericRoutes('keboola.ex-db-impala')]),
-      createComponentRoute('keboola.ex-mongodb', [exMongoDbRoutes]),
-      createComponentRoute('keboola.ex-google-bigquery', [exGoogleBigqueryRoutes]),
-      createComponentRoute('keboola.ex-teradata', [exDbGenericRoutes('keboola.ex-teradata')]),
-      createComponentRoute('keboola.csv-import', [csvImportRoutes]),
-      createComponentRoute('keboola.ex-s3', [exS3Routes]),
-      createComponentRoute('apify.apify', [exApifyRoutes]),
-      createComponentRoute('keboola.ex-aws-s3', [exAwsS3Routes]),
-      createComponentRoute('keboola.ex-email-attachments', [emailAttachmentsExtractorRoutes]),
-      createComponentRoute('keboola.ex-http', [exHttpRoutes]),
-      createComponentRoute('keboola.ex-storage', [exStorageRoutes]),
-      createComponentRoute('htns.ex-salesforce', [htnsExSalesforceRoutes]),
-      createComponentRoute('keboola.ex-ftp', [exFtpRoutes]),
-      createGenericDetailRoute('extractor')
-    ]
+    createComponentRoute('geneea-nlp-analysis', [geneeaGeneralRoutes]),
+    createComponentRoute('geneea.nlp-analysis-v2', [geneeaV2Routes]),
+    createComponentRoute('custom-science', [customScienceRoutes]),
+    createComponentRoute('keboola.app-snowflake-dwh-manager', [appSnowflakeDwhManagerRoutes]),
+    createGenericDetailRoute('application')
+  ]
+};
+
+const extractors = {
+  name: 'extractors',
+  title: 'Extractors',
+  requireData: function() {
+    return InstalledComponentsActionsCreators.loadComponents();
   },
-  writers: {
-    name: 'writers',
-    title: 'Writers',
-    requireData: function() {
-      return InstalledComponentsActionsCreators.loadComponents();
+  defaultRouteHandler: extractor(ComponentsIndex),
+  headerButtonsHandler: injectProps({
+    addRoute: 'new-extractor',
+    type: 'extractor'
+  })(ComponentsHeaderButtons),
+  reloaderHandler: ComponentReloaderButton,
+  childRoutes: [
+    {
+      name: 'new-extractor',
+      title: 'New Extractor',
+      defaultRouteHandler: extractor(NewComponent)
     },
-    defaultRouteHandler: writer(ComponentsIndex),
-    headerButtonsHandler: injectProps({
-      addRoute: 'new-writer',
-      type: 'writer'
-    })(ComponentsHeaderButtons),
-    reloaderHandler: ComponentReloaderButton,
-    childRoutes: [
-      {
-        name: 'new-writer',
-        title: 'New Writer',
-        defaultRouteHandler: writer(NewComponent)
-      },
-      createComponentRoute('gooddata-writer', [goodDataWriterRoutes]),
-      createComponentRoute('tde-exporter', [tdeRoutes]),
-      createComponentRoute('keboola.wr-google-sheets', [googleSheetsWriterRoutes]),
-      createComponentRoute('keboola.wr-google-drive', [googleDriveWriterNewRoutes]),
-      createComponentRoute('wr-db', [createDbWriterRoutes('wr-db', 'mysql', false)]),
-      createComponentRoute('wr-db-mysql', [createDbWriterRoutes('wr-db-mysql', 'mysql', false)]),
-      createComponentRoute('wr-db-oracle', [createDbWriterRoutes('wr-db-oracle', 'oracle', false)]),
-      createComponentRoute('wr-db-redshift', [createDbWriterRoutes('wr-db-redshift', 'redshift', true)]),
-      createComponentRoute('keboola.wr-looker', [createDbWriterRoutes('keboola.wr-looker', 'redshift', true)]),
-      createComponentRoute('keboola.wr-qlik', [createDbWriterRoutes('keboola.wr-qlik', 'redshift', true)]),
-      createComponentRoute('wr-tableau', [createDbWriterRoutes('wr-tableau', 'mysql', false)]),
-      createComponentRoute('wr-db-mssql', [createDbWriterRoutes('wr-db-mssql', 'mssql', false)]),
-      createComponentRoute('keboola.wr-db-mssql-v2', [createDbWriterRoutes('keboola.wr-db-mssql-v2', 'mssql', false)]),
-      createComponentRoute('keboola.wr-redshift-v2', [
-        createDbWriterRoutes('keboola.wr-redshift-v2', 'redshift', true)
-      ]),
-      createComponentRoute('keboola.wr-db-impala', [createDbWriterRoutes('keboola.wr-db-impala', 'impala', false)]),
-      createComponentRoute('keboola.wr-db-mysql', [createDbWriterRoutes('keboola.wr-db-mysql', 'mysql', false)]),
-      createComponentRoute('keboola.wr-db-oracle', [createDbWriterRoutes('keboola.wr-db-oracle', 'oracle', false)]),
-      createComponentRoute('keboola.wr-db-pgsql', [createDbWriterRoutes('keboola.wr-db-pgsql', 'pgsql', false)]),
-      createComponentRoute('keboola.wr-db-snowflake', [
-        createDbWriterRoutes('keboola.wr-db-snowflake', 'snowflake', true)
-      ]),
-      createComponentRoute('keboola.wr-thoughtspot', [
-        createDbWriterRoutes('keboola.wr-thoughtspot', 'thoughtspot', false)
-      ]),
-      createComponentRoute('keboola.gooddata-writer', [gooddataWriterV3Routes]),
-      createComponentRoute('keboola.wr-storage', [wrStorageRoutes]),
-      createComponentRoute('keboola.wr-aws-s3', [wrAwsS3Routes]),
-      createComponentRoute('keboola.wr-google-bigquery', [wrGoogleBigQueryRoutes]),
-      createComponentRoute('keboola.wr-google-bigquery-v2', [wrGoogleBigQueryV2Routes]),
-      createGenericDetailRoute('writer')
-    ]
-  }
+    createComponentRoute('keboola.ex-google-analytics-v5', [exGaV4Routes('keboola.ex-google-analytics-v5')]),
+    createComponentRoute('keboola.ex-google-analytics-v4', [exGaV4Routes('keboola.ex-google-analytics-v4')]),
+    createComponentRoute('keboola.ex-google-analytics', [exGaV4Routes('keboola.ex-google-analytics')]),
+    createComponentRoute('keboola.ex-facebook', [exFacebookRoutes('keboola.ex-facebook')]),
+    createComponentRoute('keboola.ex-facebook-ads', [exFacebookRoutes('keboola.ex-facebook-ads')]),
+    createComponentRoute('keboola.ex-instagram', [exFacebookRoutes('keboola.ex-instagram')]),
+    createComponentRoute('keboola.ex-google-drive', [exGdriveNewRoutes]),
+    createComponentRoute('ex-adform', [adformRoutes]),
+    createComponentRoute('keboola.ex-twitter', [twitterRoutes]),
+    createComponentRoute('ex-dropbox', [dropoxExtractorRoutes]),
+    createComponentRoute('radektomasek.ex-dropbox-v2', [dropoxExtractorRoutesV2]),
+    createComponentRoute('keboola.ex-db-pgsql', [exDbGenericRoutes('keboola.ex-db-pgsql')]),
+    createComponentRoute('keboola.ex-db-redshift', [exDbGenericRoutes('keboola.ex-db-redshift')]),
+    createComponentRoute('keboola.ex-db-redshift-cursors', [exDbGenericRoutes('keboola.ex-db-redshift-cursors')]),
+    createComponentRoute('keboola.ex-db-firebird', [exDbGenericRoutes('keboola.ex-db-firebird')]),
+    createComponentRoute('keboola.ex-db-db2', [exDbGenericRoutes('keboola.ex-db-db2')]),
+    createComponentRoute('keboola.ex-db-db2-bata', [exDbGenericRoutes('keboola.ex-db-db2-bata')]),
+    createComponentRoute('keboola.ex-db-mssql', [exDbGenericRoutes('keboola.ex-db-mssql')]),
+    createComponentRoute('keboola.ex-db-mysql', [exDbGenericRoutes('keboola.ex-db-mysql')]),
+    createComponentRoute('keboola.ex-db-mysql-custom', [exDbGenericRoutes('keboola.ex-db-mysql-custom')]),
+    createComponentRoute('keboola.ex-db-oracle', [exDbGenericRoutes('keboola.ex-db-oracle')]),
+    createComponentRoute('keboola.ex-db-snowflake', [exDbGenericRoutes('keboola.ex-db-snowflake')]),
+    createComponentRoute('keboola.ex-db-impala', [exDbGenericRoutes('keboola.ex-db-impala')]),
+    createComponentRoute('keboola.ex-mongodb', [exMongoDbRoutes]),
+    createComponentRoute('keboola.ex-google-bigquery', [exGoogleBigqueryRoutes]),
+    createComponentRoute('keboola.ex-teradata', [exDbGenericRoutes('keboola.ex-teradata')]),
+    createComponentRoute('keboola.csv-import', [csvImportRoutes]),
+    createComponentRoute('keboola.ex-s3', [exS3Routes]),
+    createComponentRoute('apify.apify', [exApifyRoutes]),
+    createComponentRoute('keboola.ex-aws-s3', [exAwsS3Routes]),
+    createComponentRoute('keboola.ex-email-attachments', [emailAttachmentsExtractorRoutes]),
+    createComponentRoute('keboola.ex-http', [exHttpRoutes]),
+    createComponentRoute('keboola.ex-storage', [exStorageRoutes]),
+    createComponentRoute('htns.ex-salesforce', [htnsExSalesforceRoutes]),
+    createComponentRoute('keboola.ex-ftp', [exFtpRoutes]),
+    createGenericDetailRoute('extractor')
+  ]
+};
+
+const writers = {
+  name: 'writers',
+  title: 'Writers',
+  requireData: function() {
+    return InstalledComponentsActionsCreators.loadComponents();
+  },
+  defaultRouteHandler: writer(ComponentsIndex),
+  headerButtonsHandler: injectProps({
+    addRoute: 'new-writer',
+    type: 'writer'
+  })(ComponentsHeaderButtons),
+  reloaderHandler: ComponentReloaderButton,
+  childRoutes: [
+    {
+      name: 'new-writer',
+      title: 'New Writer',
+      defaultRouteHandler: writer(NewComponent)
+    },
+    createComponentRoute('gooddata-writer', [goodDataWriterRoutes]),
+    createComponentRoute('tde-exporter', [tdeRoutes]),
+    createComponentRoute('keboola.wr-google-sheets', [googleSheetsWriterRoutes]),
+    createComponentRoute('keboola.wr-google-drive', [googleDriveWriterNewRoutes]),
+    createComponentRoute('wr-db', [createDbWriterRoutes('wr-db', 'mysql', false)]),
+    createComponentRoute('wr-db-mysql', [createDbWriterRoutes('wr-db-mysql', 'mysql', false)]),
+    createComponentRoute('wr-db-oracle', [createDbWriterRoutes('wr-db-oracle', 'oracle', false)]),
+    createComponentRoute('wr-db-redshift', [createDbWriterRoutes('wr-db-redshift', 'redshift', true)]),
+    createComponentRoute('keboola.wr-looker', [createDbWriterRoutes('keboola.wr-looker', 'redshift', true)]),
+    createComponentRoute('keboola.wr-qlik', [createDbWriterRoutes('keboola.wr-qlik', 'redshift', true)]),
+    createComponentRoute('wr-tableau', [createDbWriterRoutes('wr-tableau', 'mysql', false)]),
+    createComponentRoute('wr-db-mssql', [createDbWriterRoutes('wr-db-mssql', 'mssql', false)]),
+    createComponentRoute('keboola.wr-db-mssql-v2', [createDbWriterRoutes('keboola.wr-db-mssql-v2', 'mssql', false)]),
+    createComponentRoute('keboola.wr-redshift-v2', [
+      createDbWriterRoutes('keboola.wr-redshift-v2', 'redshift', true)
+    ]),
+    createComponentRoute('keboola.wr-db-impala', [createDbWriterRoutes('keboola.wr-db-impala', 'impala', false)]),
+    createComponentRoute('keboola.wr-db-mysql', [createDbWriterRoutes('keboola.wr-db-mysql', 'mysql', false)]),
+    createComponentRoute('keboola.wr-db-oracle', [createDbWriterRoutes('keboola.wr-db-oracle', 'oracle', false)]),
+    createComponentRoute('keboola.wr-db-pgsql', [createDbWriterRoutes('keboola.wr-db-pgsql', 'pgsql', false)]),
+    createComponentRoute('keboola.wr-db-snowflake', [
+      createDbWriterRoutes('keboola.wr-db-snowflake', 'snowflake', true)
+    ]),
+    createComponentRoute('keboola.wr-thoughtspot', [
+      createDbWriterRoutes('keboola.wr-thoughtspot', 'thoughtspot', false)
+    ]),
+    createComponentRoute('keboola.gooddata-writer', [gooddataWriterV3Routes]),
+    createComponentRoute('keboola.wr-storage', [wrStorageRoutes]),
+    createComponentRoute('keboola.wr-aws-s3', [wrAwsS3Routes]),
+    createComponentRoute('keboola.wr-google-bigquery', [wrGoogleBigQueryRoutes]),
+    createComponentRoute('keboola.wr-google-bigquery-v2', [wrGoogleBigQueryV2Routes]),
+    createGenericDetailRoute('writer')
+  ]
+};
+
+export {
+  applications,
+  extractors,
+  writers
 };
