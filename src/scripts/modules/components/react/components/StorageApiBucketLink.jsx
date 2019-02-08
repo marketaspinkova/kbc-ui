6cc09820130f63e265751e3b97a9866ac02b2506
Vladimír Kriška <vlado@keboola.com>
2019-02-08 16:49:44 +0100

    Update bucket link to point to new console when devel preview is turned on

--- src/scripts/modules/components/react/components/StorageApiBucketLink.jsx
+++ src/scripts/modules/components/react/components/StorageApiBucketLink.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import ApplicationStore from '../../../../stores/ApplicationStore';
 import { ExternalLink } from '@keboola/indigo-ui';
+import { Link } from 'react-router';
+import ApplicationStore from '../../../../stores/ApplicationStore';
+
+const UI_DEVEL_PREVIEW_FEATURE = 'ui-devel-preview';
 
 export default React.createClass({
   propTypes: {
@@ -13,6 +16,18 @@ export default React.createClass({
   },
 
   render() {
+    if (ApplicationStore.hasCurrentAdminFeature(UI_DEVEL_PREVIEW_FEATURE)) {
+      return (
+        <Link
+          to="storage-explorer-bucket"
+          params={{
+            bucketId: this.props.bucketId
+          }}
+        >
+          {this.props.children}
+        </Link>
+      );
+    }
     return (
       <ExternalLink
         href={this.bucketUrl()}
