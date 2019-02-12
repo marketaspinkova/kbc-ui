42ffab5433e0bcdc275730a60eac89d661c46172
Jakub Kotek <blaztar@gmail.com>
2019-02-12 16:01:24 +0100

    add snowflake mode

--- src/scripts/utils/codemirror/setup.js
+++ src/scripts/utils/codemirror/setup.js
@@ -3,6 +3,7 @@ import 'codemirror/mode/sql/sql';
 import 'codemirror/mode/r/r';
 import 'codemirror/mode/python/python';
 import 'codemirror/mode/diff/diff';
+import './mode/snowflake-sql';
 
 import 'codemirror/addon/display/placeholder';
 import 'codemirror/addon/hint/show-hint';
