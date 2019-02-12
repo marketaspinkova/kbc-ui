7779ec1e5eac428437f856479749266617f32d66
Vladimír Kriška <vlado@keboola.com>
2019-02-12 15:25:12 +0100

    Remove snowflake-sql references (mode is not here yet)

--- src/scripts/utils/codemirror/setup.js
+++ src/scripts/utils/codemirror/setup.js
@@ -3,7 +3,6 @@ import 'codemirror/mode/sql/sql';
 import 'codemirror/mode/r/r';
 import 'codemirror/mode/python/python';
 import 'codemirror/mode/diff/diff';
-import './mode/snowflake-sql';
 
 import 'codemirror/addon/display/placeholder';
 import 'codemirror/addon/lint/lint';
