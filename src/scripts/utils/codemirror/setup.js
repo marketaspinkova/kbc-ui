2538c6bed343f36e0da3b75cbb8f39103d1889fc
Jakub Kotek <blaztar@gmail.com>
2019-02-12 15:58:22 +0100

    add snoflake mode

--- src/scripts/utils/codemirror/setup.js
+++ src/scripts/utils/codemirror/setup.js
@@ -3,6 +3,7 @@ import 'codemirror/mode/sql/sql';
 import 'codemirror/mode/r/r';
 import 'codemirror/mode/python/python';
 import 'codemirror/mode/diff/diff';
+import '@keboola/codemirror-mode-sfsql';
 
 import 'codemirror/addon/display/placeholder';
 import 'codemirror/addon/lint/lint';
