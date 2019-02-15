9d5d4852df7ba8d8418e6e35f5773aa6dbb0e027
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2019-02-15 18:45:52 +0100

    fix: snake case to camel case in local state

--- src/scripts/modules/wr-google-bigquery-v2/adapters/serviceAccount.js
+++ src/scripts/modules/wr-google-bigquery-v2/adapters/serviceAccount.js
@@ -1,22 +1,22 @@
 import Immutable from 'immutable';
 
 const createConfiguration = function(localState) {
-  if (!localState.has('private_key')) {
+  if (!localState.has('privateKey')) {
     return Immutable.fromJS({});
   }
   const config = Immutable.fromJS({
     parameters: {
       service_account: {
         type: localState.get('type', ''),
-        project_id: localState.get('project_id', ''),
-        private_key_id: localState.get('private_key_id', ''),
-        '#private_key': localState.get('private_key', ''),
-        client_email: localState.get('client_email', ''),
-        client_id: localState.get('client_id', ''),
-        auth_uri: localState.get('auth_uri', ''),
-        token_uri: localState.get('token_uri', ''),
-        auth_provider_x509_cert_url: localState.get('auth_provider_x509_cert_url', ''),
-        client_x509_cert_url: localState.get('client_x509_cert_url', '')
+        project_id: localState.get('projectId', ''),
+        private_key_id: localState.get('privateKeyId', ''),
+        '#private_key': localState.get('privateKey', ''),
+        client_email: localState.get('clientEmail', ''),
+        client_id: localState.get('clientId', ''),
+        auth_uri: localState.get('authUri', ''),
+        token_uri: localState.get('tokenUri', ''),
+        auth_provider_x509_cert_url: localState.get('authProviderX509CertUrl', ''),
+        client_x509_cert_url: localState.get('clientX509CertUrl', '')
       }
     }
   });
@@ -26,15 +26,15 @@ const createConfiguration = function(localState) {
 const parseConfiguration = function(configuration) {
   return Immutable.fromJS({
     type: configuration.getIn(['parameters', 'service_account', 'type'], ''),
-    project_id: configuration.getIn(['parameters', 'service_account', 'project_id'], ''),
-    private_key_id: configuration.getIn(['parameters', 'service_account', 'private_key_id'], ''),
-    private_key: configuration.getIn(['parameters', 'service_account', '#private_key'], ''),
-    client_email: configuration.getIn(['parameters', 'service_account', 'client_email'], ''),
-    client_id: configuration.getIn(['parameters', 'service_account', 'client_id'], ''),
-    auth_uri: configuration.getIn(['parameters', 'service_account', 'auth_uri'], ''),
-    token_uri: configuration.getIn(['parameters', 'service_account', 'token_uri'], ''),
-    auth_provider_x509_cert_url: configuration.getIn(['parameters', 'service_account', 'auth_provider_x509_cert_url'], ''),
-    client_x509_cert_url: configuration.getIn(['parameters', 'service_account', 'client_x509_cert_url'], '')
+    projectId: configuration.getIn(['parameters', 'service_account', 'project_id'], ''),
+    privateKeyId: configuration.getIn(['parameters', 'service_account', 'private_key_id'], ''),
+    privateKey: configuration.getIn(['parameters', 'service_account', '#private_key'], ''),
+    clientEmail: configuration.getIn(['parameters', 'service_account', 'client_email'], ''),
+    clientId: configuration.getIn(['parameters', 'service_account', 'client_id'], ''),
+    authUri: configuration.getIn(['parameters', 'service_account', 'auth_uri'], ''),
+    tokenUri: configuration.getIn(['parameters', 'service_account', 'token_uri'], ''),
+    authProviderX509CertUrl: configuration.getIn(['parameters', 'service_account', 'auth_provider_x509_cert_url'], ''),
+    clientX509CertUrl: configuration.getIn(['parameters', 'service_account', 'client_x509_cert_url'], '')
   });
 };
 
--- src/scripts/modules/wr-google-bigquery-v2/adapters/serviceAccount.spec.def.js
+++ src/scripts/modules/wr-google-bigquery-v2/adapters/serviceAccount.spec.def.js
@@ -2,15 +2,15 @@ export const cases = {
   emptyWithDefaults: {
     localState: {
       type: '',
-      project_id: '',
-      private_key_id: '',
-      private_key: '',
-      client_email: '',
-      client_id: '',
-      auth_uri: '',
-      token_uri: '',
-      auth_provider_x509_cert_url: '',
-      client_x509_cert_url: ''
+      projectId: '',
+      privateKeyId: '',
+      privateKey: '',
+      clientEmail: '',
+      clientId: '',
+      authUri: '',
+      tokenUri: '',
+      authProviderX509CertUrl: '',
+      clientX509CertUrl: ''
     },
     configuration: {
       parameters: {
@@ -32,15 +32,15 @@ export const cases = {
   simple: {
     localState: {
       type: 'type',
-      project_id: 'project_id',
-      private_key_id: 'private_key_id',
-      private_key: 'private_key',
-      client_email: 'client_email',
-      client_id: 'client_id',
-      auth_uri: 'auth_uri',
-      token_uri: 'token_uri',
-      auth_provider_x509_cert_url: 'auth_provider_x509_cert_url',
-      client_x509_cert_url: 'client_x509_cert_url'
+      projectId: 'project_id',
+      privateKeyId: 'private_key_id',
+      privateKey: 'private_key',
+      clientEmail: 'client_email',
+      clientId: 'client_id',
+      authUri: 'auth_uri',
+      tokenUri: 'token_uri',
+      authProviderX509CertUrl: 'auth_provider_x509_cert_url',
+      clientX509CertUrl: 'client_x509_cert_url'
     },
     configuration: {
       parameters: {
