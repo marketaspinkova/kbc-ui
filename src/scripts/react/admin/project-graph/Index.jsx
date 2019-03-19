dbe14e04698b78932b9444846762d5d93013d9bf
Jakub Kotek <blaztar@gmail.com>
2019-03-19 13:15:26 +0100

    Add loading state

--- src/scripts/react/admin/project-graph/Index.jsx
+++ src/scripts/react/admin/project-graph/Index.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import createReactClass from 'create-react-class';
-import { getLineageInOrganization } from './GraphApi';
 import { Map, fromJS } from 'immutable';
+import { Loader } from '@keboola/indigo-ui';
+import { getLineageInOrganization } from './GraphApi';
 
 export default createReactClass({
   propTypes: {
@@ -11,30 +12,42 @@ export default createReactClass({
 
   getInitialState() {
     return {
-      lineageData: Map()
-    }
+      lineageData: Map(),
+      isLoading: false
+    };
   },
 
   componentDidMount() {
+    this.getLineageInOrganization();
+  },
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <p>
+          <Loader /> Loading data...
+        </p>
+      );
+    }
+    console.log(this.state.lineageData.toJS()); // eslint-disable-line
+    return <code>{JSON.stringify(this.state.lineageData)}</code>;
+  },
+
+  getLineageInOrganization() {
+    this.setState({ isLoading: true });
     getLineageInOrganization(
       this.props.appData.get('services').find((service) => {
         return service.get('id') === 'graph'
       }).get('url'),
-      this.props.appData.getIn(['sapi', 'token', 'token']),
+      this.props.appData.getIn(['sapi', 'token', 'token'])
     )
       .then((data) => {
         this.setState({
           lineageData: fromJS(data)
         });
+      })
+      .finally(() => {
+        this.setState({ isLoading: false });
       });
-  },
-
-  render() {
-    return (
-      <div>
-        {JSON.stringify(this.state.lineageData)}
-      </div>
-    );
   }
-
 });
