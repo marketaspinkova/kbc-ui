54197fcbed3eb5fa734dc9c6962c600d98a7848b
Vladimír Kriška <vlado@keboola.com>
2019-03-22 11:00:26 +0100

    Remove skipped "React.createElement" error

--- environment/tests/_setup/index.js
+++ environment/tests/_setup/index.js
@@ -1,12 +1,7 @@
 /* eslint-disable no-console */
 import './renderers';
 
-// Skip createElement warnings but fail tests on any other warnings and errors
 console.error = (message) => {
-  if (message.includes('React.createElement: type should not be null')) {
-    return;
-  }
-
   throw new Error(message);
 };
 
