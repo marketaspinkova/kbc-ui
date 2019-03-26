fb1a696f293e1ba6a897107299f9f7f7c7e49c8b
Jakub Kotek <blaztar@gmail.com>
2019-03-26 13:50:38 +0100

    use fixed height all the time

--- src/scripts/react/admin/project-graph/Graph.jsx
+++ src/scripts/react/admin/project-graph/Graph.jsx
@@ -36,7 +36,7 @@ export default createReactClass({
 
   initGraph() {
     this.graph = new GraphCanvas(this.prepareData(), this.refs.graph);
-    this.graph.fixedHeight = 500;
+    this.graph.height = 500;
     this.graph.zoom = { scale: 0.7, max: 1.7, min: 0.2, step: 0.1 };
     this.graph.styles = graphUtils.styles();
     this.graph.render(this.props.data.get('origin'));
