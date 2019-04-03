eea53d5d83dc7eddc5ddf2e868c6b02c3c3d4650
Jakub Kotek <blaztar@gmail.com>
2019-04-03 14:39:06 +0200

    remove react-dnd and react-dnd-html5-backend

--- src/scripts/modules/orchestrations/react/pages/orchestration-tasks/PhaseEditRow.jsx
+++ src/scripts/modules/orchestrations/react/pages/orchestration-tasks/PhaseEditRow.jsx
@@ -1,50 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import createReactClass from 'create-react-class';
-import { DragSource, DropTarget } from 'react-dnd';
 import Tooltip from '../../../../../react/common/Tooltip';
 
-const ItemType = 'PhaseRow';
-
-const phaseRowSource = {
-  beginDrag(props) {
-    return {
-      id: props.phase.get('id')
-    };
-  }
-};
-
-const phaseRowTarget = {
-  canDrop(props, monitor) {
-    const draggedId = monitor.getItem().id;
-    return draggedId === props.phase.get('id');
-  },
-  hover(props, monitor) {
-    const draggedId = monitor.getItem().id;
-    const hoverId = props.phase.get('id');
-
-    if (draggedId === hoverId) {
-      return;
-    }
-
-    props.onPhaseMove(hoverId, draggedId);
-  }
-};
-
-function collectForDragSource(connect, monitor) {
-  return {
-    connectDragSource: connect.dragSource(),
-    isDragging: monitor.isDragging()
-  };
-}
-
-function collectForDropTarget(connect) {
-  return {
-    connectDropTarget: connect.dropTarget()
-  };
-}
-
-let PhaseEditRow = createReactClass({
+export default createReactClass({
   propTypes: {
     toggleHide: PropTypes.func.isRequired,
     phase: PropTypes.object.isRequired,
@@ -55,38 +14,31 @@ let PhaseEditRow = createReactClass({
     toggleAddNewTask: PropTypes.func.isRequired,
     color: PropTypes.string,
     isPhaseHidden: PropTypes.bool,
-
-    // react-dnd
-    isDragging: PropTypes.bool.isRequired,
-    connectDragSource: PropTypes.func.isRequired,
-    connectDropTarget: PropTypes.func.isRequired
+    isDragging: PropTypes.bool
   },
 
   render() {
-    const { isDragging, connectDragSource, connectDropTarget } = this.props;
     let style = {
-      opacity: isDragging ? 0.5 : 1,
-      'backgroundColor': isDragging ? '#ffc' : this.props.color,
+      opacity: this.props.isDragging ? 0.5 : 1,
+      'backgroundColor': this.props.isDragging ? '#ffc' : this.props.color,
       cursor: 'move'
     };
     if (this.props.isPhaseHidden) {
       style.borderBottom = '2px groove';
     }
-    return connectDragSource(connectDropTarget(
-      <tr style={style} onClick={this.onRowClick}>
 
+    return (
+      <tr style={style} onClick={this.onRowClick}>
         <td>
           <div className="row">
             <div className="col-xs-3">
               <i  className="fa fa-bars"/>
-
             </div>
             <div className="col-xs-5">
               {this.renderSelectPhaseCheckbox()}
             </div>
           </div>
         </td>
-
         <td colSpan="5" className="kbc-cursor-pointer text-center">
           <div className="text-center form-group form-group-sm">
             <strong>
@@ -100,7 +52,6 @@ let PhaseEditRow = createReactClass({
             </Tooltip>
           </div>
         </td>
-
         <td>
           <div className="pull-right">
             <button
@@ -112,9 +63,8 @@ let PhaseEditRow = createReactClass({
             </button>
           </div>
         </td>
-
       </tr>
-    ));
+    );
   },
 
   renderSelectPhaseCheckbox() {
@@ -154,10 +104,4 @@ let PhaseEditRow = createReactClass({
     e.preventDefault();
     e.stopPropagation();
   }
-
 });
-
-PhaseEditRow = DragSource(ItemType, phaseRowSource, collectForDragSource)(PhaseEditRow);
-PhaseEditRow = DropTarget(ItemType, phaseRowTarget, collectForDropTarget)(PhaseEditRow);
-
-export default PhaseEditRow;
