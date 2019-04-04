2c9fbb9fb7483f63bc244160e56429393e4c32e0
Jakub Kotek <blaztar@gmail.com>
2019-04-04 10:59:02 +0200

    Use sortable - WIP

--- src/scripts/modules/orchestrations/react/pages/orchestration-tasks/TasksEditTable.jsx
+++ src/scripts/modules/orchestrations/react/pages/orchestration-tasks/TasksEditTable.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import createReactClass from 'create-react-class';
 import { Map, List, fromJS } from 'immutable';
 import underscoreString from 'underscore.string';
+import Sortable from 'react-sortablejs';
 import TasksEditTableRow from './TasksEditTableRow';
 import PhaseEditRow from './PhaseEditRow';
 import PhaseModal from '../../modals/Phase';
@@ -32,36 +33,65 @@ export default createReactClass({
   render() {
     return (
       <span>
-        {this._renderPhaseModal()}
-        {this._renderMergePhaseModal()}
-        {this._renderMoveTasksModal()}
-        {this._renderAddTaskModal()}
-        <Table responsive striped>
-          <thead>
-            <tr>
-              <th style={{ width: '10%' }}>{this.renderHeaderActionButtons()}</th>
-              <th style={{ width: '26%' }}>Component</th>
-              <th style={{ width: '26%' }}>Configuration</th>
-              <th style={{ width: '12%' }}>Action</th>
-              <th style={{ width: '8%' }}>Active</th>
-              <th style={{ width: '8%' }}>Continue on Failure</th>
-              <th style={{ width: '10%' }} />
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.tasks.count() ? (
-              this.renderPhasedTasksRows()
-            ) : (
-              <tr>
-                <td className="text-muted" colSpan="7">
-                  There are no tasks assigned yet. Please start by adding the first task.
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </Table>
+        {this.renderPhaseModal()}
+        {this.renderMergePhaseModal()}
+        {this.renderMoveTasksModal()}
+        {this.renderAddTaskModal()}
+        {this.renderTable()}
       </span>
     );
+  },
+
+  renderTable() {
+    const head = (
+      <thead>
+        <tr>
+          <th style={{ width: '10%' }}>{this.renderHeaderActionButtons()}</th>
+          <th style={{ width: '26%' }}>Component</th>
+          <th style={{ width: '26%' }}>Configuration</th>
+          <th style={{ width: '12%' }}>Action</th>
+          <th style={{ width: '8%' }}>Active</th>
+          <th style={{ width: '8%' }}>Continue on Failure</th>
+          <th style={{ width: '10%' }} />
+        </tr>
+      </thead>
+    );
+
+    if (!this.props.tasks.count()) {
+      return (
+        <Table responsive striped>
+          {head}
+          <tbody>
+            <tr>
+              <td className="text-muted" colSpan="7">
+                There are no tasks assigned yet. Please start by adding the first task.
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      )
+    }
+
+    return (
+      <Sortable
+        tag="table"
+        className="table table-striped table-responsive"
+        options={{
+          filter: 'thead',
+          handle: '.fa-bars',
+          animation: 100
+        }}
+        onChange={(order, sortable, event) => {
+          console.log(order); // eslint-disable-line
+          console.log(sortable); // eslint-disable-line
+          console.log(event); // eslint-disable-line
+        }}
+      >
+        {head}
+        {this.renderPhasedTasksRows()}
+      </Sortable>
+    );
+
   },
 
   renderHeaderActionButtons() {
@@ -143,45 +173,14 @@ export default createReactClass({
   },
 
   renderPhasedTasksRows() {
-    let result = List();
     let idx = 0;
-    this.props.tasks.map(phase => {
-      idx++;
-      const isPhaseHidden = this.isPhaseHidden(phase);
-      const color = idx % 2 > 0 ? '#fff' : '#f9f9f9'; // 'rgb(227, 248, 255)'
-      const tasksRows = phase.get('tasks').map((task, index) => {
-        const taskId = task.get('id');
-
-        return (
-          <TasksEditTableRow
-            task={task}
-            color={color}
-            component={this.props.components.get(task.get('component'))}
-            disabled={this.props.disabled}
-            key={`${index}-${taskId}`}
-            onTaskDelete={this.props.onTaskDelete}
-            onTaskUpdate={this.props.onTaskUpdate}
-            isMarked={this.props.localState.hasIn(['moveTasks', 'marked', taskId])}
-            toggleMarkTask={() => {
-              return this._toggleMarkTask(task);
-            }}
-            onMoveSingleTask={() => {
-              return this._toggleMoveSingleTask(task, phase.get('id'));
-            }}
-          />
-        );
-      });
-      const phaseRow = this.renderPhaseRow(phase, color);
-      result = result.push(phaseRow);
-      if (!isPhaseHidden) {
-        if (tasksRows.count() > 0) {
-          return (result = result.concat(tasksRows));
-        } else {
-          return (result = result.concat(this._renderEmptyTasksRow(phase.get('id'), color)));
-        }
-      }
-    });
-    return result.toArray();
+    return this.props.tasks
+      .map(phase => {
+        idx++;
+        const color = idx % 2 > 0 ? '#fff' : '#f9f9f9';
+        return this.renderPhaseRow(phase, color);
+      })
+      .toArray();
   },
 
   _renderEmptyTasksRow(phaseId, color) {
@@ -208,7 +207,7 @@ export default createReactClass({
     );
   },
 
-  _renderAddTaskModal() {
+  renderAddTaskModal() {
     return (
       <AddTaskModal
         onConfigurationSelect={this.props.handleAddTask}
@@ -225,7 +224,7 @@ export default createReactClass({
     );
   },
 
-  _renderMoveTasksModal() {
+  renderMoveTasksModal() {
     return (
       <MoveTasksModal
         show={this.props.localState.getIn(['moveTasks', 'show'], false)}
@@ -284,27 +283,65 @@ export default createReactClass({
   renderPhaseRow(phase, color) {
     const phaseId = phase.get('id');
     const isHidden = this.isPhaseHidden(phase);
+
     return (
-      <PhaseEditRow
-        isPhaseHidden={isHidden}
-        key={phaseId}
-        onPhaseMove={this.onPhaseMove}
-        phase={phase}
-        color={color}
-        toggleHide={() => {
-          return this.props.updateLocalState(['phases', phaseId, 'isHidden'], !isHidden);
-        }}
-        togglePhaseIdChange={this.togglePhaseIdEdit}
-        isMarked={this.props.localState.hasIn(['markedPhases', phaseId])}
-        onMarkPhase={this.toggleMarkPhase}
-        toggleAddNewTask={() => {
-          return this.props.updateLocalState(['newTask', 'phaseId'], phase.get('id'));
-        }}
-      />
+      <tbody key={phaseId} data-id={phaseId}>
+        <PhaseEditRow
+          isPhaseHidden={isHidden}
+          onPhaseMove={this.onPhaseMove}
+          phase={phase}
+          color={color}
+          toggleHide={() => {
+            return this.props.updateLocalState(['phases', phaseId, 'isHidden'], !isHidden);
+          }}
+          togglePhaseIdChange={this.togglePhaseIdEdit}
+          isMarked={this.props.localState.hasIn(['markedPhases', phaseId])}
+          onMarkPhase={this.toggleMarkPhase}
+          toggleAddNewTask={() => {
+            return this.props.updateLocalState(['newTask', 'phaseId'], phase.get('id'));
+          }}
+        />
+        {this._renderTasksRows(phase, color)}
+      </tbody>
     );
   },
 
-  _renderPhaseModal() {
+  _renderTasksRows(phase, color) {
+    if (this.isPhaseHidden(phase)) {
+      return null;
+    }
+
+    const tasksRows = phase.get('tasks').map((task, index) => {
+      const taskId = task.get('id');
+
+      return (
+        <TasksEditTableRow
+          task={task}
+          color={color}
+          component={this.props.components.get(task.get('component'))}
+          disabled={this.props.disabled}
+          key={`${index}-${taskId}`}
+          onTaskDelete={this.props.onTaskDelete}
+          onTaskUpdate={this.props.onTaskUpdate}
+          isMarked={this.props.localState.hasIn(['moveTasks', 'marked', taskId])}
+          toggleMarkTask={() => {
+            return this._toggleMarkTask(task);
+          }}
+          onMoveSingleTask={() => {
+            return this._toggleMoveSingleTask(task, phase.get('id'));
+          }}
+        />
+      );
+    });
+
+    if (!tasksRows.count()) {
+      return this._renderEmptyTasksRow(phase.get('id'), color);
+    }
+
+    return tasksRows;
+  },
+
+  renderPhaseModal() {
     let phaseId = this.props.localState.get('editingPhaseId');
     const existingIds = this.props.tasks.map(phase => phase.get('id')).filter(pId => pId !== phaseId);
     return (
@@ -331,7 +368,7 @@ export default createReactClass({
     return this.props.updateLocalState('mergePhases', true);
   },
 
-  _renderMergePhaseModal() {
+  renderMergePhaseModal() {
     return (
       <MergePhasesModal
         show={this.props.localState.get('mergePhases', false)}
