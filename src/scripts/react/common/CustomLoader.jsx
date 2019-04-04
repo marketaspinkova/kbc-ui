4bca09f16b0c5e87761f1fea0d89e866ee65b0cc
Vladimír Kriška <vlado@keboola.com>
2019-04-04 16:07:46 +0200

    Tune CustomLoader style a bit

--- src/scripts/react/common/CustomLoader.jsx
+++ src/scripts/react/common/CustomLoader.jsx
@@ -22,8 +22,6 @@ export default createReactClass({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: '20px',
-        maxWidth: '1000px',
         minHeight: '300px'
       }
     };
@@ -36,12 +34,14 @@ export default createReactClass({
 
     return (
       <div style={this.props.styles}>
-        <Loader
-          className={classnames({
-            'fa-2x': this.props.loaderSize === '2x',
-            'fa-4x': this.props.loaderSize === '4x'
-          })}
-        />
+        <p>
+          <Loader
+            className={classnames({
+              'fa-2x': this.props.loaderSize === '2x',
+              'fa-4x': this.props.loaderSize === '4x'
+            })}
+          />
+        </p>
         <p>{this.props.text}</p>
       </div>
     );
