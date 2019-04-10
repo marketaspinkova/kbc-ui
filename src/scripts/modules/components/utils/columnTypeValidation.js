b3b4621514dbfe198acb5de3d390cf24e7ba5429
Jakub Kotek <blaztar@gmail.com>
2019-04-10 11:16:39 +0200

    validate type si not case sensitive

--- src/scripts/modules/components/utils/columnTypeValidation.js
+++ src/scripts/modules/components/utils/columnTypeValidation.js
@@ -1,17 +1,17 @@
 export const validate = (type, value) => {
-  if (['binary', 'char', 'character', 'string', 'text', 'varchar'].includes(type)) {
+  if (['binary', 'char', 'character', 'string', 'text', 'varchar'].includes(type.toLowerCase())) {
     if (!/^(?!0)[0-9]+$/.test(value)) {
       return false;
     }
   }
 
-  if (['decimal', 'number', 'numeric', 'integer'].includes(type)) {
+  if (['decimal', 'number', 'numeric', 'integer'].includes(type.toLowerCase())) {
     if (!/^(?!0)[0-9]+(?:,?[0-9]+)?$/.test(value)) {
       return false;
     }
   }
 
-  if (type === 'time') {
+  if (type.toLowerCase() === 'time') {
     if (!/^[0-9]$/.test(value)) {
       return false;
     }
