13d53602903a999fdda0d3e80c788a7fd091daa5
Jakub Kotek <blaztar@gmail.com>
2019-05-16 21:53:26 +0200

    add more rules

--- src/scripts/modules/ex-aws-s3/adapters/rowSchema.js
+++ src/scripts/modules/ex-aws-s3/adapters/rowSchema.js
@@ -1,15 +1,25 @@
-import { boolean, object, array } from 'yup';
+import { array, boolean, object, string } from 'yup';
 
 export default object()
   .nullable()
   .shape({
-    parameters: object(),
+    parameters: object().shape({
+      bucket: string(),
+      key: string(),
+      includeSubfolders: boolean(),
+      newFilesOnly: boolean()
+    }),
     processors: object().shape({
       after: array().of(
         object().shape({
-          definition: object(),
+          definition: object().shape({
+            component: string()
+          }),
           parameters: object().shape({
+            delimiter: string(),
+            enclosure: string(),
             incremental: boolean(),
+            primary_key: array(),
             columns: array()
           })
         })
