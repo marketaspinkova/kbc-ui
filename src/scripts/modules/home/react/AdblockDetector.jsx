00c01fda60534da209755f88571e108ea2b0dfa0
Jakub Kotek <blaztar@gmail.com>
2019-05-16 12:47:24 +0200

    update text

--- src/scripts/modules/home/react/AdblockDetector.jsx
+++ src/scripts/modules/home/react/AdblockDetector.jsx
@@ -21,9 +21,14 @@ export default createReactClass({
 
     return (
       <div className="kbc-overview-component-container">
-        <AlertBlock type="warning" title="Adblock was detected">
-          The enabled adblock plugin was detected. It is strongly recommended turn it off for our
-          application. We have no ads here anyway.
+        <AlertBlock type="warning" title="Adblock detected">
+          <p>
+            We have detected that your browser is blocking advertisements. This can interfere with
+            our application and lead to seemingly missing things.
+            <br />
+            We strongly recommend to whitelist the current site ({window.location.hostname}). We
+            have no ads here anyway.
+          </p>
         </AlertBlock>
       </div>
     );
