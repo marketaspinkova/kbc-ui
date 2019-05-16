df58cb017da7b595bd560f9273baefb09b5ad795
Jakub Kotek <blaztar@gmail.com>
2019-05-16 12:02:22 +0200

    remove TODO from tde-exported ColumnRow

--- src/scripts/modules/tde-exporter/react/pages/Table/ColumnRow.jsx
+++ src/scripts/modules/tde-exporter/react/pages/Table/ColumnRow.jsx
@@ -12,19 +12,6 @@ const defaults = {
   datetime: '%Y-%m-%d %H:%M:%S'
 };
 
-/*  FORMAT HINTS TODO
-<ul>
-  <li>%Y – year (e.g. 2010)</li>
-  <li>%m – month (01 - 12)</li>
-  <li>%d – day (01 - 31)</li>
-  <li>%I – hour (01 - 12)</li>
-  <li>%H – hour 24 format (00 - 23)</li>
-  <li>%M – minutes (00 - 59)</li>
-  <li>%S – seconds (00 - 59)</li>
-  <li>%f – microsecond as a decimal number, zero-padded on the left.(000000, 000001, ..., 999999)</li>
-</ul>
-*/
-
 export default createReactClass({
   propTypes: {
     isSaving: PropTypes.bool.isRequired,
