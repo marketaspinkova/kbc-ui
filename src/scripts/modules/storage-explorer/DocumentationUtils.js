1df373a1e7112bf734190b716613d2fc7d55dc87
Tomas Kacur <tomas.kacur@keboola.com>
2019-05-24 11:16:24 +0200

    refactor DocumentationUtils: simplify createDocumentationTree

--- src/scripts/modules/storage-explorer/DocumentationUtils.js
+++ src/scripts/modules/storage-explorer/DocumentationUtils.js
@@ -1,8 +1,6 @@
 import matchByWords from '../../utils/matchByWords';
 import { List, Map } from 'immutable';
 
-
-
 const BUCKET_ROW = 'BUCKET_ROW';
 const TABLE_ROW = 'TABLE_ROW';
 const COLUMN_ROW = 'COLUMN_ROW';
@@ -35,7 +33,7 @@ function reduceDocumentationTree(documentationTree, reduceNodeFn, initValue) {
     return bucket.get('bucketTables').reduce((tablesMemo, table) => {
       const reducedTablesMemo = reduceNodeFn(tablesMemo, TABLE_ROW, table, bucket);
       return table.get('columnsDescriptions').reduce((columnsMemo, description, column) => {
-        const columnObject = Map({column, description});
+        const columnObject = Map({ column, description });
         return reduceNodeFn(columnsMemo, COLUMN_ROW, columnObject, table);
       }, reducedTablesMemo);
     }, reducedBucketMemo);
@@ -62,54 +60,64 @@ function getDescription(metadata) {
     return null;
   }
 }
-function createDocumentationTree(buckets, tables, searchQuery) {
-  return buckets.map((bucket) => {
-    const bucketId = bucket.get('id');
-    const bucketTables = tables
-      .filter((table) => table.getIn(['bucket', 'id']) === bucketId)
-      .map((table) => {
-        const columnsDescriptions = table
-          .get('columns')
-          .reduce(
-            (memo, column) =>
-              memo.set(
-                column,
-                getDescription(table.getIn(['columnMetadata', column], List()))
-              ),
-            Map()
-          ).filter((columnDescription, columnName) =>
-            matchDescriptionOrName(columnDescription, columnName, searchQuery)
-          );
-        const tableDescription = getDescription(table.get('metadata'));
-        return table
-          .set('tableDescription', tableDescription)
-          .set('columnsDescriptions', columnsDescriptions);
-      })
-      .filter(table => {
-        if (searchQuery) {
-          return matchDescriptionOrName(table.get('tableDescription'), table.get('name'), searchQuery) || table.get('columnsDescriptions').count() > 0;
-        } else {
-          return true;
-        }
-      });
-    const description = getDescription(bucket.get('metadata'));
-    return bucket
-      .set('bucketTables', bucketTables)
-      .set('bucketDescription', description);
-  }).filter(bucket => {
-    if (searchQuery) {
-      return matchDescriptionOrName(bucket.get('bucketDescription'), bucket.get('id'), searchQuery) || bucket.get('bucketTables').count() > 0;
-    } else {
-      return true;
-    }
 
-  });
+function getColumnsDescriptionsMap(table) {
+  return table
+    .get('columns')
+    .reduce(
+      (memo, column) =>
+        memo.set(column, getDescription(table.getIn(['columnMetadata', column], List()))),
+      Map()
+    );
 }
 
-const rowTypes = {BUCKET_ROW, TABLE_ROW, COLUMN_ROW};
-export {
-  createDocumentationTree,
-  buildDocumentationToMarkdown,
-  reduceDocumentationTree,
-  rowTypes
-};
+function matchBySearchQuery(searchQuery, nodeDescription, nodeName, filteredChildrenList) {
+  if (searchQuery) {
+    return (
+      matchDescriptionOrName(nodeDescription, nodeName, searchQuery) ||
+      filteredChildrenList.count() > 0
+    );
+  } else {
+    return true;
+  }
+}
+
+function createDocumentationTree(buckets, tables, searchQuery) {
+  return buckets
+    .map((bucket) => {
+      const bucketId = bucket.get('id');
+      const bucketDescription = getDescription(bucket.get('metadata'));
+      const bucketTables = tables
+        .filter((table) => table.getIn(['bucket', 'id']) === bucketId)
+        .map((table) => {
+          const tableDescription = getDescription(table.get('metadata'));
+          const columnsDescriptions = getColumnsDescriptionsMap(table).filter(
+            (columnDescription, columnName) =>
+              matchBySearchQuery(searchQuery, columnDescription, columnName, List())
+          );
+          return table
+            .set('tableDescription', tableDescription)
+            .set('columnsDescriptions', columnsDescriptions);
+        })
+        .filter((table) =>
+          matchBySearchQuery(
+            searchQuery,
+            table.get('tableDescription'),
+            table.get('name'),
+            table.get('columnsDescriptions')
+          )
+        );
+      return bucket.set('bucketTables', bucketTables).set('bucketDescription', bucketDescription);
+    })
+    .filter((bucket) =>
+      matchBySearchQuery(
+        searchQuery,
+        bucket.get('bucketDescription'),
+        bucket.get('id'),
+        bucket.get('bucketTables')
+      )
+    );
+}
+
+const rowTypes = { BUCKET_ROW, TABLE_ROW, COLUMN_ROW };
+export { createDocumentationTree, buildDocumentationToMarkdown, reduceDocumentationTree, rowTypes };
