d7acb8f651713dde536acb956f932840dd6bdd6a
Tomas Kacur <tomas.kacur@keboola.com>
2019-05-24 11:45:26 +0200

    refactor Documentation.jsx

--- src/scripts/modules/storage-explorer/react/pages/Documentation/Documentation.jsx
+++ src/scripts/modules/storage-explorer/react/pages/Documentation/Documentation.jsx
@@ -4,8 +4,8 @@ import createStoreMixin from '../../../../../react/mixins/createStoreMixin';
 import { SplitButton, MenuItem } from 'react-bootstrap';
 import NavButtons from '../../components/NavButtons';
 import { Navigation } from 'react-router';
-import ApplicationActionCreators from '../../../../../actions/ApplicationActionCreators';
 import DocumentationTable from './DocumentationTable';
+import { SearchBar, Loader } from '@keboola/indigo-ui';
 
 import BucketsStore from '../../../../components/stores/StorageBucketsStore';
 import TablesStore from '../../../../components/stores/StorageTablesStore';
@@ -13,16 +13,24 @@ import FilesStore from '../../../../components/stores/StorageFilesStore';
 import ApplicationStore from '../../../../../stores/ApplicationStore';
 import DocumentationLocalStore from '../../../DocumentationLocalStore';
 
-import { SearchBar, Loader } from '@keboola/indigo-ui';
+import { createDocumentationTree, buildDocumentationToMarkdown } from '../../../DocumentationUtils';
 
-import {createDocumentationTree, buildDocumentationToMarkdown} from '../../../DocumentationUtils';
-
-import {toggleDocumentationRow, updateDocumentationSearchQuery, uploadFile, loadLastDocumentationSnapshot } from '../../../Actions';
+import ApplicationActionCreators from '../../../../../actions/ApplicationActionCreators';
+import {
+  toggleDocumentationRow,
+  updateDocumentationSearchQuery,
+  uploadFile,
+  loadLastDocumentationSnapshot
+} from '../../../Actions';
 
 const UPLOAD_SNAPSHOT = 'upload-documentation-snapshot';
+const STORAGE_DOCUMENTATION_TAG = 'storage-documentation';
 
 export default createReactClass({
-  mixins: [createStoreMixin(BucketsStore, TablesStore, DocumentationLocalStore, FilesStore), Navigation],
+  mixins: [
+    createStoreMixin(BucketsStore, TablesStore, DocumentationLocalStore, FilesStore),
+    Navigation
+  ],
 
   getStateFromStores() {
     const allBuckets = BucketsStore.getAll().sortBy((bucket) => bucket.get('id').toLowerCase());
@@ -39,9 +47,6 @@ export default createReactClass({
   },
 
   render() {
-    const isSnapshoting = this.state.snapshotingProgress > 0 && this.state.snapshotingProgress < 100;
-    const lastSnapshot = this.state.lastSnapshot;
-
     return (
       <div className="container-fluid">
         <div className="kbc-main-content">
@@ -52,36 +57,7 @@ export default createReactClass({
               placeholder="Search in names or descriptions"
               query={this.state.searchQuery}
               onChange={updateDocumentationSearchQuery}
-              additionalActions={
-                <SplitButton
-                  id="documentation-snapshots-button"
-                  bsStyle="primary"
-                  disabled={isSnapshoting}
-                  title={
-                    <span>
-                      {isSnapshoting ? <Loader /> : <i className="fa fa-arrow-circle-o-up" />} Create Snapshot
-                    </span>
-                  }
-                  onClick={this.snapshotDocumentation}
-                  pullRight
-                >
-                  <MenuItem
-                    className="text-right"
-                    href={this.makeHref('storage-explorer-files', {}, { q: 'tags:storage-documentation' })}
-                  >
-                    Show All Snapshots
-                  </MenuItem>
-                  {lastSnapshot && (
-                    <MenuItem
-                      className="text-right"
-                      href={lastSnapshot.get('url')}
-                      target="_blank"
-                    >
-                      Load Last Snapshot
-                    </MenuItem>
-                  )}
-                </SplitButton>
-              }
+              additionalActions={this.renderSearchbarAdditionalActions()}
             />
             <DocumentationTable
               documentationTree={this.state.documentationTree}
@@ -95,21 +71,59 @@ export default createReactClass({
     );
   },
 
+  renderSearchbarAdditionalActions() {
+    const isSnapshoting =
+      this.state.snapshotingProgress > 0 && this.state.snapshotingProgress < 100;
+    const lastSnapshot = this.state.lastSnapshot;
+    return (
+      <SplitButton
+        id="documentation-snapshots-button"
+        bsStyle="primary"
+        disabled={isSnapshoting}
+        title={
+          <span>
+            {isSnapshoting ? <Loader /> : <i className="fa fa-arrow-circle-o-up" />} Create Snapshot
+          </span>
+        }
+        onClick={this.snapshotDocumentation}
+        pullRight
+      >
+        <MenuItem
+          className="text-right"
+          href={this.makeHref('storage-explorer-files', {}, { q: `tags:${STORAGE_DOCUMENTATION_TAG}` })}
+        >
+          Show All Snapshots
+        </MenuItem>
+        {lastSnapshot && (
+          <MenuItem className="text-right" href={lastSnapshot.get('url')} target="_blank">
+            Load Last Snapshot
+          </MenuItem>
+        )}
+      </SplitButton>
+    );
+  },
+
   snapshotDocumentation() {
     const currentProject = ApplicationStore.getCurrentProject();
     const projectId = currentProject.get('id');
     const projectName = currentProject.get('name');
-    const stringArray = buildDocumentationToMarkdown(this.state.documentationTree, projectId, projectName);
+    const stringArray = buildDocumentationToMarkdown(
+      this.state.documentationTree,
+      projectId,
+      projectName
+    );
     const params = {
       isPublic: true,
       isPermanent: true,
-      tags: ['storage-documentation']
+      tags: [STORAGE_DOCUMENTATION_TAG]
     };
     let file = new Blob(stringArray, { type: 'text/plain' });
     file.name = 'documentation';
 
     return uploadFile(UPLOAD_SNAPSHOT, file, params).then(() => {
-      ApplicationActionCreators.sendNotification({ message: 'Storage documentation snapshot created' });
+      ApplicationActionCreators.sendNotification({
+        message: 'Storage documentation snapshot created'
+      });
       return loadLastDocumentationSnapshot();
     });
   }
