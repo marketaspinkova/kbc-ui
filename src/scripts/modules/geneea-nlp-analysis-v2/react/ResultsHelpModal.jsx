995d65693132bdefa4a9c95c2613556db1ad4e44
marketaspinkova <41306234+marketaspinkova@users.noreply.github.com>
2019-05-28 14:15:32 +0200

    Update Help

--- src/scripts/modules/geneea-nlp-analysis-v2/react/ResultsHelpModal.jsx
+++ src/scripts/modules/geneea-nlp-analysis-v2/react/ResultsHelpModal.jsx
@@ -5,41 +5,50 @@ import {Modal} from 'react-bootstrap';
 import Markdown from '../../../react/common/Markdown';
 
 const content = `
-The result contains two tables:
+The result contains these tables:
 
-* \`analysis-result-documents\` with document-level results in the following columns:
-    * all \`id\` columns from the input table (used as primary keys)
-    * \`language\` detected language of the document, as ISO 639-1 language code
-    * \`sentimentPolarity\` detected sentiment of the document (_1_, _0_ or _-1_)
-    * \`sentimentLabel\` sentiment of the document as a label (_positive_, _neutral_ or _negative_)
-    * \`usedChars\` the number of characters used by this document
+* \`analysis-result-documents\` document-level results with the following columns:
+    * all \`id\` -- columns from the input table (used as primary keys)
+    * \`language\` -- detected language of the document, as ISO 639-1 language code
+    * \`sentimentPolarity\` -- polarity of _sentimentValue_ (_1_, _0_ or _-1_)
+    * \`sentimentLabel\` -- sentiment of the document as a label (_positive_, _neutral_ or _negative_)
+    * \`usedChars\` -- the number of characters used by this document
 
 
-* \`analysis-result-entities\` with entity-level results has the following columns:
-    * all \`id\` columns from the input table
-    * \`type\` type of the found entity, e.g. _person_, _organization_ or _tag_
-    * \`text\` disambiguated and standardized form of the entity, e.g. _John Smith_, _Keboola_, _safe carseat_
+* \`analysis-result-entities\` -- entity-level results with the following columns:
+    * all \`id\` -- columns from the input table
+    * \`type\`-- type of the found entity, e.g. _person_, _organization_ or _tag_
+    * \`text\` -- disambiguated and standardized form of the entity, e.g. _John Smith_, _Keboola_, _safe carseat_
+    * \`score\` -- expresses the importance of a tag in the text 
+    * \`entityUid\` -- ID of the entity
+    * \`sentimentValue\` -- average detected sentiment of the sentences with this entity (a decimal number between _-1_ and _1_)
+    * \`sentimentPolarity\` -- polarity of _sentimentValue_ (_1_, _0_ or _-1_)
+    * \`sentimentLabel\` -- _sentimentValue_ expressed as a label (_positive_, _neutral_ or _negative_)
 
 
 * \`analysis-result-relations\` table contains relations and attributes found in the text. For example, _good_ in _a good pizza_ or _the pizza is good_ is an attribute of _pizza_, while _eat_ in _John ate a pizza_ is a relation between _John_ and _pizza_. The table has the following columns:
+    * all \`id\` -- columns from the input table
+    * \`type\` -- \`ATTR\` for an attribute, \`VERB\` for a relation
+    * \`name\` -- the standard form of the relation
+    * \`negated\` -- \`true\` for negated relations, \`false\` otherwise
+    * \`subject\` -- the subject of the relation or target of the attribute
+    * \`object\` -- the object of the relation, if any
+    * \`subjectUid\`-- ID of the entity
+    * \`objectUid\` -- ID of the entity
+    * \`subjectType\` -- when the subject is an entity, its type (e.g. \`organization\`, \`food\`)
+    * \`objectType\` -- when the object is an entity, its type
 
-   * type -- \`ATTR\` for an attribute, \`VERB\` for a relation
-   * name -- the standard form of the relation
-   * negated -- \`true\` for negated relations, \`false\` otherwise
-   * subject -- the subject of the relation or target of the attribute
-   * object -- the object of the relation, if any
-   * subjectType -- when the subject is an entity, its type (e.g. \`organization\`, \`food\`)
-   * objectType -- when the object is an entity, its type
 
-   For _I ordered a good pizza._, the table will contain the following rows
+* \`analysis-result-sentences\` -- sentences level results with the following columns 
+    * all \`id\` -- columns from the input table
+    * \`index\` -- a zero-based index of the sentence in the document
+    * \`segment\` -- segment of the document - ``text``. ``title`` or ``lead``
+    * \`text\` -- text of the sentence
+    * \`sentimentValue\`  -- detected sentiment of the sentence (a decimal number between *-1* and *1*)
+    * \`sentimentPolarity\` -- polarity of _sentimentValue_ (_1_, _0_ or _-1_)
+    * \`sentimentLabel\` _sentimentValue_ expressed as a label (_positive_, _neutral_ or _negative_)
 
-   type | name  | negated | subject | object | subjectType | objectType |
-   ---- | ----- | ------- | ------- | ------ | ----------- | ---------- |
-   VERB | order | false   | I       | pizza  |             | food       |
-   ATTR | good  | false   | pizza   |        | food        |            |
-
-There are multiple rows per one document. All columns are part of the primary key.
-  Note that the table also contains topic tags, marked as _tag_ in the type field.
+There are usually multiple rows per one document. All columns are part of the primary key.
 `;
 
 export default createReactClass({
