07f61cfe8674f5a5c03ead6b81efcd21a4ed6200
Vladimír Kriška <vlado@keboola.com>
2019-05-30 17:12:27 +0200

    Init PendingInvitations component (copy of InvitationsButton)

---
+++ src/scripts/react/admin/projects-list/PendingInvitations.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import createReactClass from 'create-react-class';
+
+export default createReactClass({
+
+  propTypes: {
+    invitationsCount: PropTypes.number.isRequired
+  },
+
+  pluralText() {
+    return this.props.invitationsCount > 1 ? 's' : null;
+  },
+
+  render() {
+    if (!this.props.invitationsCount) {
+      return null;
+    }
+
+    return (
+      <a
+        href="/admin/account/projects-invitations"
+        className="btn btn-lg btn-block btn-warning text-left action-button"
+      >
+        <span className="raquo">&raquo;</span>
+        <strong className="h2">Pending Invitations</strong><br/>
+        <span className="small">
+          You have <strong>{this.props.invitationsCount}</strong> pending
+          {' '}<strong>invitation{this.pluralText()}</strong><br/>
+          {' '}waiting for your response.
+        </span>
+      </a>
+    );
+  }
+});
