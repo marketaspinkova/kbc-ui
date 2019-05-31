7b877920632f90c8369d316a3d1845af58ad5d80
pivnicek <marc@keboola.com>
2019-05-31 04:28:36 +0200

    add option to disable fallback for mssql-ex"

--- src/scripts/modules/ex-db-generic/templates/customFields.js
+++ src/scripts/modules/ex-db-generic/templates/customFields.js
@@ -8,13 +8,25 @@ const noLockField = {
   showInAdvancedMode: false
 };
 
+const disableFallbackField = {
+  label: 'Disable Fallback',
+  name: 'disableFallback',
+  type: 'checkbox',
+  protected: false,
+  required: false,
+  help: 'Disable the PDO fallback method on error',
+  showInAdvancedMode: false
+};
+
 const ALL_FIELDS = [
-  noLockField
+  noLockField,
+  disableFallbackField
 ];
 
 const CUSTOM_FIELDS = {
   'keboola.ex-db-mssql': [
-    noLockField
+    noLockField,
+    disableFallbackField
   ]
 };
 
