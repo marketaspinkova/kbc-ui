a52078f52d75591173566357a15eaf6b54dbbbba
Vladimír Kriška <vlado@keboola.com>
2019-06-04 09:36:52 +0200

    Use feature definition from constanst file

--- src/scripts/modules/ex-db-generic/utils.js
+++ src/scripts/modules/ex-db-generic/utils.js
@@ -1,9 +1,11 @@
 import ApplicationStore from '../../stores/ApplicationStore';
+import { FEATURE_PGSQL_SPLIT_LOADING } from '../../constants/KbcConstants';
 
 const supported = ['keboola.ex-db-pgsql'];
 
 const supportSplitLoading = (componentId) => {
-  return supported.includes(componentId) && ApplicationStore.hasCurrentProjectFeature('pgsql-split-loading');
+  return supported.includes(componentId)
+    && ApplicationStore.hasCurrentProjectFeature(FEATURE_PGSQL_SPLIT_LOADING);
 }
 
 export {
